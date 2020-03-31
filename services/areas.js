f27a7e5180a2c3d5d582b48bf50fd3746a838eb4
sandi sunandar <sandisunandar99@gmail.com>
2020-03-31 17:18:23 +0700

    area sorting asc by name

--- services/areas.js
+++ services/areas.js
@@ -20,7 +20,7 @@ function getDistrictCity(request, callback) {
   }
 
   Districtcity.find(params)
-    .sort({ kemendagri_kabupaten_kode: 'asc' })
+    .sort({ kemendagri_kabupaten_name: 'asc' })
     .exec()
     .then(city => {
         let res = city.map(q => q.toJSONFor())
@@ -38,7 +38,7 @@ function getSubDistrict(city_code, request, callback) {
   }
 
   SubDistrict.find(params)
-    .sort({ kemendagri_kecamatan_kode: 'asc' })
+    .sort({ kemendagri_kecamatan_name: 'asc' })
     .exec()
     .then(distric => {
         let res = distric.map(q => q.toJSONFor())
@@ -50,7 +50,7 @@ function getSubDistrict(city_code, request, callback) {
 function getSubDistrictDetail(kecamatan_kode, callback) {
   console.log(kecamatan_kode)
   SubDistrict.find({ kemendagri_kecamatan_kode: kecamatan_kode})
-    .sort({ kemendagri_kecamatan_kode: 'asc' })
+    .sort({ kemendagri_kecamatan_name: 'asc' })
     .exec()
     .then(distric => {
         let res = distric.map(q => q.toJSONFor())
@@ -68,7 +68,7 @@ function getVillage(kecamatan_code, request, callback) {
   }
 
   Village.find(params)
-      .sort({ kemendagri_desa_kode: 'asc' })
+      .sort({ kemendagri_desa_name: 'asc' })
       .exec()
       .then(vill => {
           let res = vill.map(q => q.toJSONFor())
@@ -79,7 +79,7 @@ function getVillage(kecamatan_code, request, callback) {
 
 function getVillageDetail(desa_kode, callback) {
   Village.find({ kemendagri_desa_kode: desa_kode })
-      .sort({ kemendagri_desa_kode: 'asc' })
+      .sort({ kemendagri_desa_name: 'asc' })
       .exec()
       .then(vill => {
           let res = vill.map(q => q.toJSONFor())
