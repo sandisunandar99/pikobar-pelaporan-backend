cd6a0ff4b05fb95145ff37656dccb1bd6637f979
sandi sunandar <sandisunandar99@gmail.com>
2020-04-03 15:25:31 +0700

    count data case for generate id_case in rdt

--- api/rdt/routes.js
+++ api/rdt/routes.js
@@ -11,8 +11,9 @@ module.exports = (server) =>{
     const countRdtCode = require('./route_prerequesites').countRdtCode(server)
     const getRdtbyId = require('./route_prerequesites').getRdtbyId(server)
     const getCodeDinkes = require('./route_prerequesites').getCodeDinkes(server)
-    const countCaseByDistrict = require('./route_prerequesites').countCaseByDistrict(server)
     const checkIfDataNotNull = require('./route_prerequesites').checkIfDataNotNull(server)
+    
+    const countCaseByDistrict = require('../cases/route_prerequesites').countCaseByDistrict(server)
 
 
     return [
