5b944190d18ff0a77c331d31f8682bdcd9b2c20e
firmanjs <fimzhakim@gmail.com>
2020-04-08 15:19:46 +0700

    change status deleted

--- helpers/rolecheck.js
+++ helpers/rolecheck.js
@@ -19,21 +19,21 @@ const listByRole = (user, params, search_params, schema) => {
   let result_search
   if (search_params == null) {
     if(user.role == 'dinkeskota'){
-      result_search = schema.find(params).where('status').ne('deleted')
+      result_search = schema.find(params).where('delete_status').ne('deleted')
     }else if (user.role == 'dinkesprov' || user.role == 'superadmin') {
-      result_search = schema.find(params).where('status').ne('deleted')
+      result_search = schema.find(params).where('delete_status').ne('deleted')
     } else {
       result_search = schema.find({
         'author': user._id
-      }).where('status').ne('deleted')
+      }).where('delete_status').ne('deleted')
     }
   } else {
     if (user.role == 'dinkeskota' || user.role == 'dinkesprov' || user.role == 'superadmin') {
-      result_search = schema.find(params).or(search_params).where('status').ne('deleted')
+      result_search = schema.find(params).or(search_params).where('delete_status').ne('deleted')
     } else {
       result_search = schema.find({
         'author': user._id
-      }).or(search_params).where('status').ne('deleted')
+      }).or(search_params).where('delete_status').ne('deleted')
     }
   }
   return result_search
