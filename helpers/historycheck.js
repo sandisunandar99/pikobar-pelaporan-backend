4d58c05148501b17171e84a9a0eba52d3eb77ee4
firmanjs <fimzhakim@gmail.com>
2020-04-14 12:45:44 +0700

    logic history download

--- helpers/historycheck.js
+++ helpers/historycheck.js
@@ -26,9 +26,9 @@ const historyCheck = (history) => {
         }else{
             notes = ""
         }
-        var a = (abroad != "" ? "," : "")
-        var b = (city != "" ? "," : "")
-        var c = (positive != "" ? "," : "")
+        var a = (city != "" ? "," : "")
+        var b = (positive != "" ? "," : "")
+        var c = (notes != "" ? "," : "")
     }
     return `${abroad}${a}${city}${b}${positive}${c}${notes}`
 }
