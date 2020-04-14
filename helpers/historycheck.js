0676aaab5ece68636bb893c48d5a432424ec48e4
firmanjs <fimzhakim@gmail.com>
2020-04-14 13:04:37 +0700

    null validation

--- helpers/historycheck.js
+++ helpers/historycheck.js
@@ -1,8 +1,8 @@
 
 const historyCheck = (history) => {
     let abroad,city,positive,notes
-    if(history !== ""){
-
+    if(history !== null){
+        
         if(history.is_went_abroad == true){
             abroad = `Dari Luar Negeri Mengunjungi Negara ${history.visited_country}`
         }else{
