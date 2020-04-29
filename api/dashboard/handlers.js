2c03a08ae033bbb72d2b10e147e108bd9a137465
sandi sunandar <sandisunandar99@gmail.com>
2020-04-29 15:32:17 +0700

    add handler PDP, OTG, ODP

--- api/dashboard/handlers.js
+++ api/dashboard/handlers.js
@@ -26,5 +26,51 @@ module.exports = (server) => {
                 }
             )
         },
+        /**
+         * GET /api/dashboard
+         * @param {*} request
+         * @param {*} reply
+         */
+        async countODP(request, reply) {
+            server.methods.services.dashboard.countByODP(
+                request.query,
+                request.auth.credentials.user,
+                (err, result) => {
+                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+                    return reply(dashboardResponse(result)).code(200);
+                }
+            )
+        },
+        /**
+         * GET /api/dashboard
+         * @param {*} request
+         * @param {*} reply
+         */
+        async countPDP(request, reply) {
+            server.methods.services.dashboard.countByPDP(
+                request.query,
+                request.auth.credentials.user,
+                (err, result) => {
+                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+                    return reply(dashboardResponse(result)).code(200);
+                }
+            )
+        },
+        /**
+         * GET /api/dashboard
+         * @param {*} request
+         * @param {*} reply
+         */
+        async countOTG(request, reply) {
+            server.methods.services.dashboard.countByOTG(
+                request.query,
+                request.auth.credentials.user,
+                (err, result) => {
+                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+                    return reply(dashboardResponse(result)).code(200);
+                }
+            )
+        },
+
     } //end
 }
