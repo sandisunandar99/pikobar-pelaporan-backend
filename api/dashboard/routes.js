40f3ab4764ede53af2baf7ce2696fc5d409fd59d
sandi sunandar <sandisunandar99@gmail.com>
2020-04-29 15:32:33 +0700

    add and remark endpoitn

--- api/dashboard/routes.js
+++ api/dashboard/routes.js
@@ -1,13 +1,45 @@
 module.exports = (server) => {
     const handlers = require('./handlers')(server)
-    return [{
-        method: 'GET',
-        path: '/dashboard',
-        config: {
-            auth: 'jwt',
-            description: 'show dashboard statistik',
-            tags: ['api', 'dashboard statistik'],
+    return [
+        {
+            method: 'GET',
+            path: '/dashboard/chart-age-gender',
+            config: {
+                auth: 'jwt',
+                description: 'show dashboard statistik',
+                tags: ['api', 'dashboard statistik'],
+            },
+            handler: handlers.countGender,
         },
-        handler: handlers.countGender,
-    }]
+        {
+            method: 'GET',
+            path: '/dashboard/chart-odp',
+            config: {
+                auth: 'jwt',
+                description: 'show dashboard statistik',
+                tags: ['api', 'dashboard statistik'],
+            },
+            handler: handlers.countODP,
+        },
+        {
+            method: 'GET',
+            path: '/dashboard/chart-pdp',
+            config: {
+                auth: 'jwt',
+                description: 'show dashboard statistik',
+                tags: ['api', 'dashboard statistik'],
+            },
+            handler: handlers.countPDP,
+        },
+        {
+            method: 'GET',
+            path: '/dashboard/chart-otg',
+            config: {
+                auth: 'jwt',
+                description: 'show dashboard statistik',
+                tags: ['api', 'dashboard statistik'],
+            },
+            handler: handlers.countOTG,
+        }
+]
 }
