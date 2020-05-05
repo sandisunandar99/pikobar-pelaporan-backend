38ebcf0d062b9d54ac3e65802162bb20a6f2a569
sandi sunandar <sandisunandar99@gmail.com>
2020-05-05 16:32:43 +0700

    add cumulative and fix formulation for ODP, PDP , OTG

--- services/dashboard.js
+++ services/dashboard.js
@@ -16,14 +16,89 @@ const countByGenderAge = async (query, user, callback) => {
   } catch (error) {
     callback(error, null);
   }
-};
-
+}
 
 const countByOdp = async (query, user, callback) => {
   try {
     const queryODP = await Sql.sqlCondition(user, query, "ODP");
     const result = await Case.aggregate(queryODP);
-    callback(null, result);
+
+    let get_date = []
+    result.forEach((val, key) => {
+      get_date.push(new Date(val.date))
+    })
+    
+    var maxDate = new Date(Math.max.apply(null, get_date));
+    var minDate = new Date(Math.min.apply(null, get_date));
+    
+    let date_range =[]
+    for (let index = minDate; index <= maxDate; index.setDate(index.getDate() + 1)){
+        var mm = ((index.getMonth() + 1) >= 10) ? (index.getMonth() + 1) : '0' + (index.getMonth() + 1);
+        var dd = ((index.getDate()) >= 10) ? (index.getDate()) : '0' + (index.getDate());
+        var yyyy = index.getFullYear();
+        var date = yyyy+"/"+mm+"/"+dd
+        date_range.push(date)
+    }
+
+    let get_result_date = []
+    result.forEach((val, key) => {
+      get_result_date.push(val.date)
+    })
+
+    let result2 = []
+    date_range.forEach((val, key) =>{
+      if (!get_result_date.includes(val)) {
+        result2.push({
+          proses: 0,
+          selesai: 0,
+          total: 0,
+          date: val
+        })
+      }
+    })
+
+    let res = result2.concat(result)
+    res.sort((a, b) => {
+      var dateA = new Date(a.date),
+        dateB = new Date(b.date)
+      return dateA - dateB //sort by date ascending
+    })
+
+    let cum_proses =[]
+    let cum_selesai =[]
+    res.forEach((val, key) =>{
+      cum_proses.push(val.proses)
+      cum_selesai.push(val.selesai)
+    })
+    
+    let triger_selesai = 0
+    let result_cum_selesai =[]
+    cum_selesai.map(x => {
+      triger_selesai += x
+      result_cum_selesai.push({
+        cum_selesai: triger_selesai
+      })
+    })
+  
+
+    let triger_proses = 0
+    let result_cum_proses = []
+    cum_proses.map(z => {
+      triger_proses += z
+      result_cum_proses.push({
+        cum_proses: triger_proses
+      })
+    })
+
+    let cum_proses_selesai = []
+    res.forEach((val, key)=>{
+      val = Object.assign(val,result_cum_proses[key])
+      val = Object.assign(val,result_cum_selesai[key])
+      cum_proses_selesai.push(val)
+    })
+    
+
+    callback(null, cum_proses_selesai);
   } catch (error) {
     callback(error, null);
   }
@@ -33,18 +108,168 @@ const countByPdp = async (query, user, callback) => {
   try {
     const queryODP = await Sql.sqlCondition(user, query, "PDP");
     const result = await Case.aggregate(queryODP);
-    callback(null, result);
+
+       let get_date = []
+       result.forEach((val, key) => {
+         get_date.push(new Date(val.date))
+       })
+
+       var maxDate = new Date(Math.max.apply(null, get_date));
+       var minDate = new Date(Math.min.apply(null, get_date));
+
+       let date_range = []
+       for (let index = minDate; index <= maxDate; index.setDate(index.getDate() + 1)) {
+         var mm = ((index.getMonth() + 1) >= 10) ? (index.getMonth() + 1) : '0' + (index.getMonth() + 1);
+         var dd = ((index.getDate()) >= 10) ? (index.getDate()) : '0' + (index.getDate());
+         var yyyy = index.getFullYear();
+         var date = yyyy + "/" + mm + "/" + dd
+         date_range.push(date)
+       }
+
+       let get_result_date = []
+       result.forEach((val, key) => {
+         get_result_date.push(val.date)
+       })
+
+       let result2 = []
+       date_range.forEach((val, key) => {
+         if (!get_result_date.includes(val)) {
+           result2.push({
+             proses: 0,
+             selesai: 0,
+             total: 0,
+             date: val
+           })
+         }
+       })
+
+      let res = result2.concat(result)
+      res.sort((a, b) => {
+        var dateA = new Date(a.date),
+          dateB = new Date(b.date)
+        return dateA - dateB //sort by date ascending
+      })
+
+      let cum_proses = []
+      let cum_selesai = []
+      res.forEach((val, key) => {
+        cum_proses.push(val.proses)
+        cum_selesai.push(val.selesai)
+      })
+
+      let triger_selesai = 0
+      let result_cum_selesai = []
+      cum_selesai.map(x => {
+        triger_selesai += x
+        result_cum_selesai.push({
+          cum_selesai: triger_selesai
+        })
+      })
+
+
+      let triger_proses = 0
+      let result_cum_proses = []
+      cum_proses.map(z => {
+        triger_proses += z
+        result_cum_proses.push({
+          cum_proses: triger_proses
+        })
+      })
+
+      let cum_proses_selesai = []
+      res.forEach((val, key) => {
+        val = Object.assign(val, result_cum_proses[key])
+        val = Object.assign(val, result_cum_selesai[key])
+        cum_proses_selesai.push(val)
+      })
+
+
+    callback(null, cum_proses_selesai);
   } catch (error) {
     callback(error, null);
   }
 }
 
-
 const countByOtg = async (query, user, callback) => {
   try {
     const queryOtg = await Sql.sqlCondition(user, query, "OTG");
     const result = await Case.aggregate(queryOtg);
-    callback(null, result);
+
+       let get_date = []
+       result.forEach((val, key) => {
+         get_date.push(new Date(val.date))
+       })
+
+       var maxDate = new Date(Math.max.apply(null, get_date));
+       var minDate = new Date(Math.min.apply(null, get_date));
+
+       let date_range = []
+       for (let index = minDate; index <= maxDate; index.setDate(index.getDate() + 1)) {
+         var mm = ((index.getMonth() + 1) >= 10) ? (index.getMonth() + 1) : '0' + (index.getMonth() + 1);
+         var dd = ((index.getDate()) >= 10) ? (index.getDate()) : '0' + (index.getDate());
+         var yyyy = index.getFullYear();
+         var date = yyyy + "/" + mm + "/" + dd
+         date_range.push(date)
+       }
+
+       let get_result_date = []
+       result.forEach((val, key) => {
+         get_result_date.push(val.date)
+       })
+
+       let result2 = []
+       date_range.forEach((val, key) => {
+         if (!get_result_date.includes(val)) {
+           result2.push({
+             proses: 0,
+             selesai: 0,
+             total: 0,
+             date: val
+           })
+         }
+       })
+
+      let res = result2.concat(result)
+      res.sort((a, b) => {
+        var dateA = new Date(a.date),
+          dateB = new Date(b.date)
+        return dateA - dateB //sort by date ascending
+      })
+
+      let cum_proses = []
+      let cum_selesai = []
+      res.forEach((val, key) => {
+        cum_proses.push(val.proses)
+        cum_selesai.push(val.selesai)
+      })
+
+      let triger_selesai = 0
+      let result_cum_selesai = []
+      cum_selesai.map(x => {
+        triger_selesai += x
+        result_cum_selesai.push({
+          cum_selesai: triger_selesai
+        })
+      })
+
+
+      let triger_proses = 0
+      let result_cum_proses = []
+      cum_proses.map(z => {
+        triger_proses += z
+        result_cum_proses.push({
+          cum_proses: triger_proses
+        })
+      })
+
+      let cum_proses_selesai = []
+      res.forEach((val, key) => {
+        val = Object.assign(val, result_cum_proses[key])
+        val = Object.assign(val, result_cum_selesai[key])
+        cum_proses_selesai.push(val)
+      })
+
+    callback(null, cum_proses_selesai);
   } catch (error) {
     callback(error, null);
   }
