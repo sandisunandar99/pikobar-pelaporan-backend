3c8b18c412efa85186cae65cc36270125e027d5b
sandi sunandar <sandisunandar99@gmail.com>
2020-05-08 14:42:54 +0700

    service table agregate criteria

--- services/dashboard.js
+++ services/dashboard.js
@@ -2,9 +2,50 @@ require('../models/Case');
 const Mongoose = require('mongoose');
 const Helpers = require('../helpers/dashboardbottom');
 const Case = Mongoose.model('Case');
+const DistrictCity = Mongoose.model('Districtcity')
 const Sql = require('../helpers/sectionnumber');
 const Check = require('../helpers/rolecheck');
 const Filter = require('../helpers/casefilter');
+
+const summaryAggregateByDinkes = async (query, user, callback) =>{
+  try {
+    let querySummary = await Sql.summaryAgregatePerDinkes(user, query)
+    let result = await Case.aggregate(querySummary)
+    let kab_kota = await DistrictCity.find({'kemendagri_provinsi_kode':'32'}) 
+
+    let getKabkotaCodeAndName = []
+    kab_kota.forEach((val, key)=>{
+      getKabkotaCodeAndName.push({
+        kab_kota: val.kemendagri_kabupaten_kode,
+        kab_kota_name: val.kemendagri_kabupaten_nama
+      })
+    })
+    
+    let combine_data = []
+    result.forEach((val, key) =>{
+        getKabkotaCodeAndName.forEach((val1,key1) =>{
+            if (val.kab_kota === val1.kab_kota) {
+              combine_data.push({
+                odp_proses:val.odp_proses,
+                pdp_proses:val.pdp_proses,
+                otg_proses:val.otg_proses,
+                positif_aktif:val.positif_aktif,
+                positif_sembuh:val.positif_sembuh,
+                positif_meninggal:val.positif_meninggal,
+                total: val.total,
+                kab_kota: val.kab_kota,
+                kab_kota_name: val1.kab_kota_name
+              })
+            }
+        })
+    })
+  
+  
+    return callback(null, combine_data)
+  } catch (error) {
+    callback(error, null)
+  }
+}
 
 
 const countByGenderAge = async (query, user, callback) => {
@@ -24,23 +65,23 @@ const countByGenderAge = async (query, user, callback) => {
 const countByOdp = async (query, user, callback) => {
   try {
     const queryODP = await Sql.sqlCondition(user, query, "ODP");
-    const result = await Case.aggregate(queryODP);
+    const result = await Case.aggregate(queryODP)
 
     let get_date = []
     result.forEach((val, key) => {
       get_date.push(new Date(val.date))
     })
-    
+
     var maxDate = new Date(Math.max.apply(null, get_date));
     var minDate = new Date(Math.min.apply(null, get_date));
-    
-    let date_range =[]
-    for (let index = minDate; index <= maxDate; index.setDate(index.getDate() + 1)){
-        var mm = ((index.getMonth() + 1) >= 10) ? (index.getMonth() + 1) : '0' + (index.getMonth() + 1);
-        var dd = ((index.getDate()) >= 10) ? (index.getDate()) : '0' + (index.getDate());
-        var yyyy = index.getFullYear();
-        var date = yyyy+"/"+mm+"/"+dd
-        date_range.push(date)
+
+    let date_range = []
+    for (let index = minDate; index <= maxDate; index.setDate(index.getDate() + 1)) {
+      var mm = ((index.getMonth() + 1) >= 10) ? (index.getMonth() + 1) : '0' + (index.getMonth() + 1);
+      var dd = ((index.getDate()) >= 10) ? (index.getDate()) : '0' + (index.getDate());
+      var yyyy = index.getFullYear();
+      var date = yyyy + "/" + mm + "/" + dd
+      date_range.push(date)
     }
 
     let get_result_date = []
@@ -49,7 +90,7 @@ const countByOdp = async (query, user, callback) => {
     })
 
     let result2 = []
-    date_range.forEach((val, key) =>{
+    date_range.forEach((val, key) => {
       if (!get_result_date.includes(val)) {
         result2.push({
           proses: 0,
@@ -67,22 +108,22 @@ const countByOdp = async (query, user, callback) => {
       return dateA - dateB //sort by date ascending
     })
 
-    let cum_proses =[]
-    let cum_selesai =[]
-    res.forEach((val, key) =>{
+    let cum_proses = []
+    let cum_selesai = []
+    res.forEach((val, key) => {
       cum_proses.push(val.proses)
       cum_selesai.push(val.selesai)
     })
-    
+
     let triger_selesai = 0
-    let result_cum_selesai =[]
+    let result_cum_selesai = []
     cum_selesai.map(x => {
       triger_selesai += x
       result_cum_selesai.push({
         cum_selesai: triger_selesai
       })
     })
-  
+
 
     let triger_proses = 0
     let result_cum_proses = []
@@ -92,17 +133,14 @@ const countByOdp = async (query, user, callback) => {
         cum_proses: triger_proses
       })
     })
-    console.log(result_cum_proses);
-    console.log(triger_proses);
-    
 
     let cum_proses_selesai = []
-    res.forEach((val, key)=>{
-      val = Object.assign(val,result_cum_proses[key])
-      val = Object.assign(val,result_cum_selesai[key])
+    res.forEach((val, key) => {
+      val = Object.assign(val, result_cum_proses[key])
+      val = Object.assign(val, result_cum_selesai[key])
       cum_proses_selesai.push(val)
     })
-    
+
 
     callback(null, cum_proses_selesai);
   } catch (error) {
@@ -402,5 +440,9 @@ module.exports = [
   {
     name: "services.dashboard.countByConfirm",
     method: countByConfirm
+  },
+  {
+    name: "services.dashboard.summaryAggregateByDinkes",
+    method: summaryAggregateByDinkes
   },
 ]
