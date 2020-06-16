b0709c837ef22a152b2899ca914cf02507321e3e
firmanjs <fimzhakim@gmail.com>
2020-06-16 14:53:22 +0700

    case related map

---
+++ services/case_related.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+require('../models/Case');
+const Case = mongoose.model('Case');
+const Check = require('../helpers/rolecheck');
+const Filter = require('../helpers/casefilter');
+const _ = require('lodash')
+
+const listCaseRelated = async (query, user, callback) => {
+    try {
+        const whereRole = Check.countByRole(user);
+        const filter = await Filter.filterCase(user, query);
+        const condition = Object.assign(whereRole, filter);
+        const staticParam = {
+            "delete_status":{"$ne":"deleted"},
+            "id_case_related":{"$nin":[null,"",'']},
+        };
+        const searching = Object.assign(condition, staticParam);
+        const res = await Case.find({$or: [searching]});
+        const result = _.groupBy(res, 'id_case_related');
+        callback(null, result);
+    } catch (error) {
+        callback(error, null);
+    }
+}
+
+module.exports = [
+    {
+        name: 'services.case_related.list',
+        method: listCaseRelated
+    }
+];
+
