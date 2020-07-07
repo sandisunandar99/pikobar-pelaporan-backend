581f6cf34d1f5cbb9fd794e1b7adba88a10c15d9
firmanjs <fimzhakim@gmail.com>
2020-07-07 12:49:51 +0700

    minor string logic

--- helpers/cases/revamp/handlerpost.js
+++ helpers/cases/revamp/handlerpost.js
@@ -1,5 +1,6 @@
 'use strict';
 const { CASE, ROLE } = require('../../constant');
+const moment = require('moment');
 const validatePost = (raw_payload) => {
     if (raw_payload.current_hospital_id === "") {
         raw_payload.current_hospital_id = null;
@@ -13,17 +14,21 @@ const validatePost = (raw_payload) => {
 };
 
 const generateIdCase = (author, pre) => {
-    let dates = new Date().getFullYear().toString().substring(2);
+    let dates = moment(new Date()).format("YY");
     let id_case;
     let pasienCount ='';
     let pendingCount = '';
+    let faskesCode = pre.count_case_pending.count_pasien.toString();
+    let maskedNumber = faskesCode.length.padStart(5, 0);
+    let otherCOde = pre.count_case.count_pasien.toString();
+    let otherMaskedNumber = otherCOde.length.padStart(4, 0);
     let dinkesCode = pre.count_case_pending.dinkes_code;
     if (author.role === ROLE.FASKES) {
-        pasienCount = "0".repeat(5 - pre.count_case_pending.count_pasien.toString().length);
+        pasienCount = maskedNumber;
         pendingCount = pre.count_case_pending.count_pasien;
         id_case = `${CASE.PRE}${dinkesCode}${dates}${pasienCount}${pendingCount}`;
     } else {
-        pasienCount = "0".repeat(4 - pre.count_case.count_pasien.toString().length);
+        pasienCount = otherMaskedNumber;
         pendingCount = pre.count_case.count_pasien;
         id_case = `${CASE.CODE}${dinkesCode}${dates}${pasienCount}${pendingCount}`;
     }
