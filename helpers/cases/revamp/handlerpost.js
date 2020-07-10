f2cf41bc6d438de6f8e7fdea4b9f3f5fdb15297e
firmanjs <fimzhakim@gmail.com>
2020-07-10 16:07:52 +0700

    change logic generate case

--- helpers/cases/revamp/handlerpost.js
+++ helpers/cases/revamp/handlerpost.js
@@ -1,5 +1,4 @@
 'use strict';
-const Cases = require('../../../models/Case');
 const { CASE, ROLE } = require('../../constant');
 const moment = require('moment');
 const validatePost = (raw_payload) => {
@@ -14,39 +13,26 @@ const validatePost = (raw_payload) => {
     return raw_payload;
 };
 
-const generateIdCase = async (author, pre) => {
+const generateIdCase = (author, pre) => {
     let dates = moment(new Date()).format("YY");
     let id_case;
-    let finalCount;
-    let finalIdCase;
     let pasienCount ='';
     let pendingCount = '';
     let dinkesCode = pre.count_case_pending.dinkes_code;
     let zero = '0';
     let faskesCode = pre.count_case_pending.count_pasien.toString().length;
     let otherCode = pre.count_case.count_pasien.toString().length;
-    /* check jika id kasus sudah ada pengecekan ini berlaku 
-       jika user menginput yang bukan wilayahnya
-    */
     if (author.role === ROLE.FASKES) {
-        pasienCount = zero.padStart(5 - faskesCode, 0);
+        pasienCount = zero.repeat(5 - faskesCode);
         pendingCount = pre.count_case_pending.count_pasien;
         id_case = `${CASE.PRE}${dinkesCode}${dates}${pasienCount}${pendingCount}`;
-        finalCount = await checkIdCase(id_case, pendingCount);
-        finalIdCase = `${CASE.PRE}${dinkesCode}${dates}${pasienCount}${finalCount}`;
     } else {
-        pasienCount = zero.padStart(4 - otherCode, 0);
+        pasienCount = zero.repeat(4 - otherCode);
         pendingCount = pre.count_case.count_pasien;
         id_case = `${CASE.CODE}${dinkesCode}${dates}${pasienCount}${pendingCount}`;
-        finalCount = await checkIdCase(id_case, pendingCount);
-        finalIdCase = `${CASE.CODE}${dinkesCode}${dates}${pasienCount}${finalCount}`;
     }
-    return finalIdCase;
-}
 
-const checkIdCase = async (id_case, pendingCount) => {
-    const checkData =  await Cases.findOne({'id_case':id_case}).countDocuments();
-    return (checkData ? Number(pendingCount) + 1 : pendingCount);
+    return id_case;
 }
 
 module.exports = {
