1a89d970453dbbf1eec9b48c62088df7a48e4244
khihadysucahyo <khihady.ks@gmail.com>
2020-09-24 21:00:17 +0700

    delete unused closecontactv2 func

--- api/close_contact/route_prerequesites.js
+++ api/close_contact/route_prerequesites.js
@@ -46,29 +46,6 @@ const getCloseContactbyId = server => {
     }
 }
 
-const getContactCaseById = server => {
-  return {
-      method: (request, reply) => {
-           let id = request.params.contactCaseId
-           server.methods.services.cases
-              .getById(id, (err, result) => {
-                  if (err) {
-                      return reply(replyHelper.constructErrorResponse(err)).code(422).takeover()
-                  }
-                  if (!result) {
-                      return reply({
-                          status: 422,
-                          message: 'Invalid contact case id',
-                          data: null
-                      }).code(422).takeover()
-                  }
-                  return reply(result)
-              })
-      },
-      assign: 'contactCase'
-  }
-}
-
 const districtInputScope = server => {
     return {
         method: (request, reply) => {
@@ -86,43 +63,8 @@ const districtInputScope = server => {
     }
 }
 
-const isAccessGranted = server => {
-  return {
-    method: (request, reply) => {
-      let isAccessGranted = false
-      const currentCase = request.preResponses.cases.source
-      const contactCase = request.preResponses.contactCase.source
-      const attrs = [ 'close_contact_parents', 'close_contact_childs' ]
-
-      attrs.forEach(key => {
-        if (currentCase[key]) {
-          const havingGrantAccess = currentCase[key].find(v => {
-            return v.id_case === contactCase.id_case && v.is_access_granted
-          })
-
-          if (havingGrantAccess) {
-            isAccessGranted = true
-          }
-        }
-      })
-
-      if (!isAccessGranted) {
-        return reply({
-          status: 422,
-          message: "this current active case haven't an access to this related case"
-        }).code(422).takeover()
-      }
-
-      return reply(true)
-      },
-      assign: 'is_access_granted'
-  }
-}
-
 module.exports = {
     getCasebyId,
-    getContactCaseById,
     getCloseContactbyId,
     districtInputScope,
-    isAccessGranted,
 }
