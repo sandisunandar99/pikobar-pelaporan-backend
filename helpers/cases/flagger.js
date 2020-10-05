3f250289b850f3f217262983995abd82f9014cbe
khihadysucahyo <khihady.ks@gmail.com>
2020-10-05 17:39:32 +0700

    try to fix cognitive complexity

--- helpers/cases/flagger.js
+++ helpers/cases/flagger.js
@@ -25,7 +25,7 @@ const doFlagging = async (source, self, Case) => {
 
   if (closeconProps.includes(prop)) {
     if (pre) return
-    return handleClosecontactFlag(Case, cond, prop)
+    return handleClosecontactFlag(Case, cond.id_case, prop)
   }
 
   if (!id || !prop) return
@@ -63,41 +63,39 @@ const doFlagging = async (source, self, Case) => {
 
 }
 
-const handleClosecontactFlag = async (Case, cond, prop) => {
-  const idCase = cond.id_case
+const handleClosecontactFlag = async (Case, idCase, prop) => {
+  if (!idCase || !prop) return
+
   const rules = { id_case: idCase }
 
-  if (idCase && prop) {
+  const record = await Case
+    .findOne(rules)
+    .select(closeconProps)
 
-    const record = await Case
-      .findOne(rules)
-      .select(closeconProps)
+  if (!record) return
 
-    if (!record) return
+  const {
+    close_contact_childs,
+    close_contact_parents,
+  } = record
 
-    const {
-      close_contact_childs,
-      close_contact_parents,
-    } = record
+  let status = 0, childs = null, parents = null;
 
-    let status = 0, childs = null, parents = null;
-
-    if (close_contact_childs) {
-      childs = close_contact_childs.filter(v => !!v.is_access_granted)
-    }
-
-    if (close_contact_parents) {
-      parents = close_contact_parents.filter(v => !!v.is_access_granted)
-    }
-
-    if (childs.length || parents.length) {
-      status = 1
-    }
-
-    return await Case.updateOne(rules,{
-      $set: { status_closecontact: status }
-    })
+  if (close_contact_childs) {
+    childs = close_contact_childs.filter(v => !!v.is_access_granted)
   }
+
+  if (close_contact_parents) {
+    parents = close_contact_parents.filter(v => !!v.is_access_granted)
+  }
+
+  if (childs.length || parents.length) {
+    status = 1
+  }
+
+  return await Case.updateOne(rules,{
+    $set: { status_closecontact: status }
+  })
 }
 
 module.exports = {
