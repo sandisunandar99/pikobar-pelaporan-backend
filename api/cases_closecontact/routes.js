bff7d0cc0dd9479c6b1704ec83b4ac07d4d4e48e
khihadysucahyo <khihady.ks@gmail.com>
2020-10-06 16:20:27 +0700

    rename

--- api/cases_closecontact/routes.js
+++ api/cases_closecontact/routes.js
@@ -4,7 +4,7 @@ module.exports = (server) =>{
   const getContactCaseById = require('./route_prerequesites').getContactCaseById(server)
   const isAccessGranted = require('./route_prerequesites').isAccessGranted(server)
 
-  const apiPath = (method, path, callback) => {
+  const route = (method, path, callback) => {
     return {
       method: method,
       path: path,
@@ -19,11 +19,11 @@ module.exports = (server) =>{
   }
 
   return [
-    apiPath('GET', '/cases/{caseId}/closecontact', 'ListClosecontactCase'),
-    apiPath('POST', '/cases/{caseId}/closecontact', 'CreateClosecontact'),
-    apiPath('PUT', '/cases/{caseId}/closecontact', 'updateClosecontact'),
-    apiPath('GET', '/cases/{caseId}/closecontact/{contactCaseId}', 'DetailClosecontact'),
-    apiPath('PUT', '/cases/{caseId}/closecontact/{contactCaseId}', 'UpdateClosecontact'),
-    apiPath('DELETE', '/cases/{caseId}/closecontact/{contactCaseId}', 'DeleteClosecontact'),
+    route('GET', '/cases/{caseId}/closecontact', 'ListClosecontactCase'),
+    route('POST', '/cases/{caseId}/closecontact', 'CreateClosecontact'),
+    route('PUT', '/cases/{caseId}/closecontact', 'updateClosecontact'),
+    route('GET', '/cases/{caseId}/closecontact/{contactCaseId}', 'DetailClosecontact'),
+    route('PUT', '/cases/{caseId}/closecontact/{contactCaseId}', 'UpdateClosecontact'),
+    route('DELETE', '/cases/{caseId}/closecontact/{contactCaseId}', 'DeleteClosecontact'),
   ]
 }
