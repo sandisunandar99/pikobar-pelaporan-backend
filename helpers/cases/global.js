fcc945a30fa9a10f577a5cf2aa08ef53ad0768fe
khihadysucahyo <khihady.ks@gmail.com>
2020-10-16 10:11:14 +0700

    pre to doc

--- helpers/cases/global.js
+++ helpers/cases/global.js
@@ -1,6 +1,6 @@
 // requirement func to generate id case to support multiple insertion cases
 const getCountBasedOnDistrict = async (services, districtCode) => {
-  const pre = {
+  const doc = {
     count_case: {},
     count_case_pending: {},
     case_count_outside_west_java: {},
@@ -10,24 +10,24 @@ const getCountBasedOnDistrict = async (services, districtCode) => {
     districtCode,
     (err, res) => {
       if (err) return
-      pre.count_case = res
+      doc.count_case = res
     })
 
   await services.cases.getCountPendingByDistrict(
     districtCode,
     (err, res) => {
       if (err) return
-      pre.count_case_pending = res
+      doc.count_case_pending = res
     })
 
   await services.v2.cases.getCaseCountsOutsideWestJava(
     districtCode,
     (err, res) => {
       if (err) return
-      pre.case_count_outside_west_java = res
+      doc.case_count_outside_west_java = res
     })
 
-  return pre
+  return doc
 }
 
 module.exports = {
