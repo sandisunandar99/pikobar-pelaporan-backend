7789f4f47fc7aaaf75cd566c087a6aae2c5bd87e
firmanjs <fimzhakim@gmail.com>
2020-10-20 14:15:38 +0700

    dictionary

---
+++ helpers/dictionary/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "LABEL": {
+    "BIRTH_DATE": "Tanggal Lahir",
+    "FIRST_SYMTOM_DATE": "Tanggal awal timbul gejala",
+  },
+  "MESSAGES": {
+    "UNVERIFIED_TEMPLATE": "Template Kasus Yang diimport tidak dikenali, Silahkan download template terbaru",
+    "VERSION_OUT_DATE": "Versi template tidak didukung, Silahkan download template terbaru",
+    "INVALID_DATE_FORMAT": "harus dalam format (tttt-bb-hh)",
+    "INVALID_STATUS": "\"Stage\" harus berisi [PROSES/SELESAI]",
+    "INVALID_STAGE": "\"Status\" harus berisi [OTG/PDP/POSITIF]",
+    "INVALID_FINAL_RESULT": "\"Status\" harus berisi [POSITIF/SEMBUH/MENINGGAL]"
+  }
+}
