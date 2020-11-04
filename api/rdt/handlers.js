619bf3bc234ca82ba7eb1a73dbbcad0fcf24fbb3
sandi <sandisunandar99@gmail.com>
2020-11-04 11:08:12 +0700

    fix handler dan route

--- api/rdt/handlers.js
+++ api/rdt/handlers.js
@@ -1,211 +1,211 @@
 const replyHelper = require('../helpers')
+const {
+  replyJson
+} = require('../helpers')
 
-module.exports = (server) => {
-    function constructRdtResponse(rdt) {
-        let jsonRdt = {
-            status: 200,
-            message: "Success",
-            data: rdt
-        }
-        return jsonRdt
-    }
-
-    return {
-        async ListRdt(request, reply){
-            server.methods.services.rdt.list(
-                request.query,
-                request.auth.credentials.user,
-                (err, result) => {
-                if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                return reply(
-                    constructRdtResponse(result,request)
-                ).code(200)
-            })
-        },
-
-        async CreateRdt(request, reply){
-            server.methods.services.rdt.create(
-                request.query,
-                request.payload,
-                request.auth.credentials.user,
-                request.pre,
-                  (err, result) => {
-                  if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                  return reply(
-                      constructRdtResponse(result)
-                  ).code(200)
-                }
-            )
-        },
-
-        async CreateRdtMultiple(request, reply){
-            server.methods.services.rdt.createMultiple(
-                request.payload,
-                request.auth.credentials.user,
-                request.pre,
-                  (err, result) => {
-                  if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                  return reply(
-                      constructRdtResponse(result)
-                  ).code(200)
-                }
-            )
-        },
-
-        async GetRdtDetail(request, reply) {
-            server.methods.services.rdt.getById(request.params.id, (err, item) => {
-                if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                return reply(
-                    constructRdtResponse(item)
-                ).code(200)
-            })
-        },
-
-        async GetRdtHistories(request, reply) {
-            server.methods.services.rdt.getHistoriesByRdtId(request.params.id, (err, item) => {
-                if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                return reply(
-                    constructRdtResponse(item)
-                ).code(200)
-            })
-        },
-
-        async UpdateRdt(request, reply){
-            server.methods.services.rdt.update(
-                request.params.id, request.payload, request.auth.credentials.user,
-                (err, result) => {
-                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                    return reply(
-                        constructRdtResponse(result)
-                    ).code(200)
-                }
-            )
-        },
-
-        async DeleteRdt(request, reply) {
-            server.methods.services.rdt.softDeleteRdt(
-                request.pre.rdt,
-                // request.pre.cases,
-                request.auth.credentials.user,
-                (err, item) => {
-                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                     return reply(
-                         constructRdtResponse(item)
-                     ).code(202)
-                })
-        },
-
-        async GetListIdCase(request, reply) {
-            server.methods.services.rdt.FormSelectIdCase(
-                request.query,
-                request.auth.credentials.user,
-                request.pre.data_pendaftaran,
-                (err, result) => {
-                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                    return reply(
-                        constructRdtResponse(result, request)
-                    ).code(200)
-            })
-        },
-
-        async GetListIdCaseDetail(request, reply) {
-            server.methods.services.rdt.FormSelectIdCaseDetail(
-                request.pre.search_internal,
-                request.pre.search_external,
-                // request.auth.credentials.user,
-                (err, result) => {
-                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                    return reply(
-                        constructRdtResponse(result, request)
-                    ).code(200)
-            })
-        },
-
-        async GetListRegisteredUser(request, reply) {
-            server.methods.services.rdt.getRegisteredUser(
-                request.pre.reg_user_external,
-                request.auth.credentials.user,
-                (err, result) => {
-                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                    return reply(
-                        constructRdtResponse(result, request)
-                    ).code(200)
-            })
-        },
-
-        async formLocationTest(request, reply) {
-            server.methods.services.rdt.getLocationTest(
-                (err, result) => {
-                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                    return reply(
-                        constructRdtResponse(result, request)
-                    ).code(200)
-            })
-        },
-
-        async GetRdtSummaryByCities(request, reply) {
-              server.methods.services.rdt.GetRdtSummaryByCities(
-                  request.query,
-                  (err, result) => {
-                      if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                      return reply(
-                          constructRdtResponse(result, request)
-                      ).code(200)
-                  })
-        },
-
-        async GetRdtSummaryResultByCities(request, reply) {
-              server.methods.services.rdt.GetRdtSummaryResultByCities(
-                  request.query,
-                  (err, result) => {
-                      if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                      return reply(
-                          constructRdtResponse(result, request)
-                      ).code(200)
-                  })
-        },
-
-        async GetRdtSummaryResultListByCities(request, reply) {
-              server.methods.services.rdt.GetRdtSummaryResultListByCities(
-                  request.query,
-                  (err, result) => {
-                      if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                      return reply(
-                          constructRdtResponse(result, request)
-                      ).code(200)
-                  })
-        },
-
-        async GetRdtFaskesSummaryByCities(request, reply) {
-              server.methods.services.rdt.GetRdtFaskesSummaryByCities(
-                  request.query,
-                  (err, result) => {
-                      if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                      return reply(
-                          constructRdtResponse(result, request)
-                      ).code(200)
-                  })
-        },
-
-        async sendMessage(result) {
-            server.methods.services.rdt.sendMessagesSMS(
-                request.query,
-                (err, result) => {
-                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                    return reply(
-                        constructRdtResponse(result, request)
-                    ).code(200)
-                })
-            server.methods.services.rdt.sendMessagesWA(
-                request.query,
-                (err, result) => {
-                    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-                    return reply(
-                        constructRdtResponse(result, request)
-                    ).code(200)
-                })
-        }
-
-    }//end
-
+const ListRdt = (server) => {
+  return (request, reply) => {
+    server.methods.services.rdt.list(
+      request.query,
+      request.auth.credentials.user,
+      (err, result) => {
+        replyJson(err, result, reply)
+      })
+  }
 }
+
+const CreateRdt = (server) => {
+  return (request, reply) => {
+    server.methods.services.rdt.create(
+      request.query,
+      request.payload,
+      request.auth.credentials.user,
+      request.pre,
+      (err, result) => {
+        replyJson(err, result, reply)
+      }
+    )
+  }
+}
+
+const CreateRdtMultiple = (server) => {
+  return (request, reply) => {
+    server.methods.services.rdt.createMultiple(
+      request.payload,
+      request.auth.credentials.user,
+      request.pre,
+      (err, result) => {
+        replyJson(err, result, reply)
+      }
+    )
+  }
+}
+
+const GetRdtDetail = (server) => {
+  return (request, reply) => {
+    server.methods.services.rdt.getById(request.params.id, (err, result) => {
+      replyJson(err, result, reply)
+    })
+  }
+}
+
+
+module.exports = {
+  ListRdt,
+  CreateRdt,
+  CreateRdtMultiple,
+  GetRdtDetail
+}
+
+
+// module.exports = (server) => {
+//     return {
+
+//         async GetRdtHistories(request, reply) {
+//             server.methods.services.rdt.getHistoriesByRdtId(request.params.id, (err, item) => {
+//                 if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                 return reply(
+//                     constructRdtResponse(item)
+//                 ).code(200)
+//             })
+//         },
+
+//         async UpdateRdt(request, reply){
+//             server.methods.services.rdt.update(
+//                 request.params.id, request.payload, request.auth.credentials.user,
+//                 (err, result) => {
+//                     if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                     return reply(
+//                         constructRdtResponse(result)
+//                     ).code(200)
+//                 }
+//             )
+//         },
+
+//         async DeleteRdt(request, reply) {
+//             server.methods.services.rdt.softDeleteRdt(
+//                 request.pre.rdt,
+//                 // request.pre.cases,
+//                 request.auth.credentials.user,
+//                 (err, item) => {
+//                     if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                      return reply(
+//                          constructRdtResponse(item)
+//                      ).code(202)
+//                 })
+//         },
+
+//         async GetListIdCase(request, reply) {
+//             server.methods.services.rdt.FormSelectIdCase(
+//                 request.query,
+//                 request.auth.credentials.user,
+//                 request.pre.data_pendaftaran,
+//                 (err, result) => {
+//                     if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                     return reply(
+//                         constructRdtResponse(result, request)
+//                     ).code(200)
+//             })
+//         },
+
+//         async GetListIdCaseDetail(request, reply) {
+//             server.methods.services.rdt.FormSelectIdCaseDetail(
+//                 request.pre.search_internal,
+//                 request.pre.search_external,
+//                 // request.auth.credentials.user,
+//                 (err, result) => {
+//                     if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                     return reply(
+//                         constructRdtResponse(result, request)
+//                     ).code(200)
+//             })
+//         },
+
+//         async GetListRegisteredUser(request, reply) {
+//             server.methods.services.rdt.getRegisteredUser(
+//                 request.pre.reg_user_external,
+//                 request.auth.credentials.user,
+//                 (err, result) => {
+//                     if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                     return reply(
+//                         constructRdtResponse(result, request)
+//                     ).code(200)
+//             })
+//         },
+
+//         async formLocationTest(request, reply) {
+//             server.methods.services.rdt.getLocationTest(
+//                 (err, result) => {
+//                     if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                     return reply(
+//                         constructRdtResponse(result, request)
+//                     ).code(200)
+//             })
+//         },
+
+//         async GetRdtSummaryByCities(request, reply) {
+//               server.methods.services.rdt.GetRdtSummaryByCities(
+//                   request.query,
+//                   (err, result) => {
+//                       if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                       return reply(
+//                           constructRdtResponse(result, request)
+//                       ).code(200)
+//                   })
+//         },
+
+//         async GetRdtSummaryResultByCities(request, reply) {
+//               server.methods.services.rdt.GetRdtSummaryResultByCities(
+//                   request.query,
+//                   (err, result) => {
+//                       if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                       return reply(
+//                           constructRdtResponse(result, request)
+//                       ).code(200)
+//                   })
+//         },
+
+//         async GetRdtSummaryResultListByCities(request, reply) {
+//               server.methods.services.rdt.GetRdtSummaryResultListByCities(
+//                   request.query,
+//                   (err, result) => {
+//                       if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                       return reply(
+//                           constructRdtResponse(result, request)
+//                       ).code(200)
+//                   })
+//         },
+
+//         async GetRdtFaskesSummaryByCities(request, reply) {
+//               server.methods.services.rdt.GetRdtFaskesSummaryByCities(
+//                   request.query,
+//                   (err, result) => {
+//                       if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                       return reply(
+//                           constructRdtResponse(result, request)
+//                       ).code(200)
+//                   })
+//         },
+
+//         async sendMessage(result) {
+//             server.methods.services.rdt.sendMessagesSMS(
+//                 request.query,
+//                 (err, result) => {
+//                     if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                     return reply(
+//                         constructRdtResponse(result, request)
+//                     ).code(200)
+//                 })
+//             server.methods.services.rdt.sendMessagesWA(
+//                 request.query,
+//                 (err, result) => {
+//                     if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+//                     return reply(
+//                         constructRdtResponse(result, request)
+//                     ).code(200)
+//                 })
+//         }
+
+//     }//end
+
+// }
--- api/rdt/routes.js
+++ api/rdt/routes.js
@@ -1,5 +1,5 @@
 module.exports = (server) =>{
-    const handlers = require('./handlers')(server)
+    const handlers = require('./handlers')
     const inputValidations = require('./validations/input')
     //const outputValidations = require('./validations/output')
 
@@ -21,61 +21,61 @@ module.exports = (server) =>{
     const validationBeforeInput = require('./route_prerequesites').validationBeforeInput(server)
     const cekHistoryCases = require('./route_prerequesites').cekHistoryCases(server)
     const createHistoryWhenPositif = require('./route_prerequesites').createHistoryWhenPositif(server)
-    
+
     return [
         // Get list case for form
-        {
-            method: 'GET',
-            path: '/rdt/list-idcase',
-            config: {
-                auth: 'jwt',
-                description: 'show list id_case for form',
-                tags: ['api', 'rdt'],
-                validate: inputValidations.rdtSearchValidation,
-                pre: [
-                    getDataExternal
-                ]
-            },
-            handler: handlers.GetListIdCase
-        },
-        {
-            method: 'GET',
-            path: '/rdt/list-idcase-detail',
-            config: {
-                auth: 'jwt',
-                description: 'show list id_case detil',
-                tags: ['api', 'rdt'],
-                pre:[
-                    searchIdcasefromInternal,
-                    searchIdcasefromExternal
-                ]
-            },
-            handler: handlers.GetListIdCaseDetail
-        },
-        {
-            method: 'GET',
-            path: '/rdt/list-registered-user',
-            config: {
-                auth: 'jwt',
-                description: 'show list id_case detil',
-                tags: ['api', 'rdt'],
-                pre:[
-                    getRegisteredUserfromExternal
-                ]
-            },
-            handler: handlers.GetListRegisteredUser
-        },
-        {
-            method: 'GET',
-            path: '/rdt/list-location-test',
-            config: {
-                auth: 'jwt',
-                description: 'show list location test for form multiple input rdt',
-                tags: ['api', 'rdt'],
-            },
-            handler: handlers.formLocationTest
-        },
-        // Get list rdt
+        // {
+        //     method: 'GET',
+        //     path: '/rdt/list-idcase',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'show list id_case for form',
+        //         tags: ['api', 'rdt'],
+        //         validate: inputValidations.rdtSearchValidation,
+        //         pre: [
+        //             getDataExternal
+        //         ]
+        //     },
+        //     handler: handlers.GetListIdCase
+        // },
+        // {
+        //     method: 'GET',
+        //     path: '/rdt/list-idcase-detail',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'show list id_case detil',
+        //         tags: ['api', 'rdt'],
+        //         pre:[
+        //             searchIdcasefromInternal,
+        //             searchIdcasefromExternal
+        //         ]
+        //     },
+        //     handler: handlers.GetListIdCaseDetail
+        // },
+        // {
+        //     method: 'GET',
+        //     path: '/rdt/list-registered-user',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'show list id_case detil',
+        //         tags: ['api', 'rdt'],
+        //         pre:[
+        //             getRegisteredUserfromExternal
+        //         ]
+        //     },
+        //     handler: handlers.GetListRegisteredUser
+        // },
+        // {
+        //     method: 'GET',
+        //     path: '/rdt/list-location-test',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'show list location test for form multiple input rdt',
+        //         tags: ['api', 'rdt'],
+        //     },
+        //     handler: handlers.formLocationTest
+        // },
+        // // Get list rdt
         {
             method: 'GET',
             path: '/rdt',
@@ -90,7 +90,7 @@ module.exports = (server) =>{
                     checkIfDataNotNull
                 ]
             },
-            handler: handlers.ListRdt
+            handler: handlers.ListRdt(server)
         },
         // Create rdt
         {
@@ -110,7 +110,7 @@ module.exports = (server) =>{
                     createHistoryWhenPositif
                 ]
             },
-            handler: handlers.CreateRdt
+            handler: handlers.CreateRdt(server)
         },
         {
             method: 'POST',
@@ -123,7 +123,7 @@ module.exports = (server) =>{
                     CheckRoleCreate
                 ]
             },
-            handler: handlers.CreateRdtMultiple
+            handler: handlers.CreateRdtMultiple(server)
         },
         // Get detail rdt
         {
@@ -137,107 +137,107 @@ module.exports = (server) =>{
                     CheckRoleView
                 ]
             },
-            handler: handlers.GetRdtDetail
+            handler: handlers.GetRdtDetail(server)
         },
 
-        // Get rdt hostories
-        {
-            method: 'GET',
-            path: '/rdt/{id}/histories',
-            config: {
-                auth: 'jwt',
-                description: 'show a specific rdt details',
-                tags: ['api', 'rdt'],
-                pre: [
-                    CheckRoleView
-                ]
-            },
-            handler: handlers.GetRdtHistories
-        },
+        // // Get rdt hostories
+        // {
+        //     method: 'GET',
+        //     path: '/rdt/{id}/histories',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'show a specific rdt details',
+        //         tags: ['api', 'rdt'],
+        //         pre: [
+        //             CheckRoleView
+        //         ]
+        //     },
+        //     handler: handlers.GetRdtHistories
+        // },
 
-        // Update rdt
-        {
-            method: 'PUT',
-            path: '/rdt/{id}',
-            config: {
-                auth: 'jwt',
-                description: 'update rdt',
-                tags: ['api', 'rdt'],
-                pre: [
-                    CheckRoleUpdate
-                ]
-            },
-            handler: handlers.UpdateRdt
-        },
-        {
-            method: 'DELETE',
-            path: '/rdt/{id}',
-            config: {
-                auth: 'jwt',
-                description: 'soft delete by id rdt',
-                tags: ['api', 'rdt'],
-                pre: [
-                    CheckRoleDelete,
-                    getRdtbyId
-                    // getCasebyIdcase
-                ]
-            },
-            handler: handlers.DeleteRdt
-        },
+        // // Update rdt
+        // {
+        //     method: 'PUT',
+        //     path: '/rdt/{id}',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'update rdt',
+        //         tags: ['api', 'rdt'],
+        //         pre: [
+        //             CheckRoleUpdate
+        //         ]
+        //     },
+        //     handler: handlers.UpdateRdt
+        // },
+        // {
+        //     method: 'DELETE',
+        //     path: '/rdt/{id}',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'soft delete by id rdt',
+        //         tags: ['api', 'rdt'],
+        //         pre: [
+        //             CheckRoleDelete,
+        //             getRdtbyId
+        //             // getCasebyIdcase
+        //         ]
+        //     },
+        //     handler: handlers.DeleteRdt
+        // },
 
-        // Get RDT Test summary by cities
-        {
-            method: 'GET',
-            path: '/rdt/summary-by-cities',
-            config: {
-                auth: 'jwt',
-                description: 'Get RDT Test summary by cities',
-                tags: ['api', 'rdt']
-            },
-            handler: handlers.GetRdtSummaryByCities
-        },
+        // // Get RDT Test summary by cities
+        // {
+        //     method: 'GET',
+        //     path: '/rdt/summary-by-cities',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'Get RDT Test summary by cities',
+        //         tags: ['api', 'rdt']
+        //     },
+        //     handler: handlers.GetRdtSummaryByCities
+        // },
 
-        // Get RDT result (positif, negatif, invalid) summary by cities
-        {
-            method: 'GET',
-            path: '/rdt/summary-result-by-cities',
-            config: {
-                auth: 'jwt',
-                description: 'Get RDT result summary by cities',
-                tags: ['api', 'rdt'],
-                pre: [
-                    CheckRoleView
-                ]
-            },
-            handler: handlers.GetRdtSummaryResultByCities
-        },
+        // // Get RDT result (positif, negatif, invalid) summary by cities
+        // {
+        //     method: 'GET',
+        //     path: '/rdt/summary-result-by-cities',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'Get RDT result summary by cities',
+        //         tags: ['api', 'rdt'],
+        //         pre: [
+        //             CheckRoleView
+        //         ]
+        //     },
+        //     handler: handlers.GetRdtSummaryResultByCities
+        // },
 
-        // Get RDT used + result (positif, negatif, invalid) summary by cities
-        {
-            method: 'GET',
-            path: '/rdt/summary-result-list-by-cities',
-            config: {
-                auth: 'jwt',
-                description: 'Get RDT result summary list by cities',
-                tags: ['api', 'rdt'],
-                pre: [
-                    CheckRoleView
-                ]
-            },
-            handler: handlers.GetRdtSummaryResultListByCities
-        },
+        // // Get RDT used + result (positif, negatif, invalid) summary by cities
+        // {
+        //     method: 'GET',
+        //     path: '/rdt/summary-result-list-by-cities',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'Get RDT result summary list by cities',
+        //         tags: ['api', 'rdt'],
+        //         pre: [
+        //             CheckRoleView
+        //         ]
+        //     },
+        //     handler: handlers.GetRdtSummaryResultListByCities
+        // },
 
-        // Get RDT Test Faskes summary by cities
-        {
-            method: 'GET',
-            path: '/rdt/faskes-summary-by-cities',
-            config: {
-                auth: 'jwt',
-                description: 'Get RDT Test summary by cities',
-                tags: ['api', 'rdt']
-            },
-            handler: handlers.GetRdtFaskesSummaryByCities
-        },
+        // // Get RDT Test Faskes summary by cities
+        // {
+        //     method: 'GET',
+        //     path: '/rdt/faskes-summary-by-cities',
+        //     config: {
+        //         auth: 'jwt',
+        //         description: 'Get RDT Test summary by cities',
+        //         tags: ['api', 'rdt']
+        //     },
+        //     handler: handlers.GetRdtFaskesSummaryByCities
+        // },
 
         // send message sms and whatsapps
         //  {
