7cec74862644f50bce4c0f81f4d9079963ee7f4b
sandi <sandisunandar99@gmail.com>
2020-11-16 15:58:37 +0700

    fix histories

--- api/histories/routes.js
+++ api/histories/routes.js
@@ -3,85 +3,15 @@ module.exports = (server) =>{
     const CheckRoleView = require('../users/route_prerequesites').CheckRoleView(server);
     const CheckRoleCreate = require('../users/route_prerequesites').CheckRoleCreate(server);
     const CheckRoleUpdate = require('../users/route_prerequesites').CheckRoleUpdate(server);
+    const {configRouteComplete} = require('../../helpers/routes')
+
 
     return [
-        {
-            method: 'GET',
-            path: '/history_cases',
-            config: {
-                auth: 'jwt',
-                description: 'show list of all histories',
-                tags: ['api', 'histories'],
-                pre: [
-                    CheckRoleView
-                ]
-            },
-            handler: handlers.ListHistory(server)
-        },
-        {
-            method: 'POST',
-            path: '/history_cases',
-            config: {
-                auth: 'jwt',
-                description: 'create new histories',
-                tags: ['api', 'histories'],
-                pre: [
-                    CheckRoleCreate
-                ]
-            },
-            handler: handlers.CreateHistory(server)
-        },
-        {
-            method: 'GET',
-            path: '/history_cases/{id}',
-            config: {
-                auth: 'jwt',
-                description: 'show a specific histories details',
-                tags: ['api', 'histories'],
-                pre: [
-                    CheckRoleView
-                ]
-            },
-            handler: handlers.GetHistoryDetail(server)
-        },
-        {
-          method: 'PUT',
-          path: '/history_cases/{id}',
-          config: {
-            auth: 'jwt',
-            description: 'update specific histories',
-            tags: ['api', 'histories'],
-            pre: [
-              CheckRoleUpdate,
-            ]
-          },
-          handler: handlers.UpdateHistory(server)
-        },
-        {
-          method: 'DELETE',
-          path: '/history_cases/{id}',
-          config: {
-            auth: 'jwt',
-            description: 'delete specific histories',
-            tags: ['api', 'histories'],
-            pre: [
-              CheckRoleUpdate,
-            ]
-          },
-          handler: handlers.DeleteHistory(server)
-        },
-        {
-          method: 'GET',
-          path: '/history-export',
-          config: {
-              auth: 'jwt',
-              description: 'export histories',
-              tags: ['api', 'histories'],
-              pre: [
-                  CheckRoleView
-              ]
-          },
-          handler: handlers.exportHistory(server)
-        },
+        configRouteComplete('GET', '/history_cases', null, [CheckRoleView], 'Histories', handlers.ListHistory(server)),
+        configRouteComplete('POST', '/history_cases', null, [CheckRoleCreate], 'Histories', handlers.CreateHistory(server)),
+        configRouteComplete('GET', '/history_cases/{id}', null, [CheckRoleView], 'Histories', handlers.GetHistoryDetail(server)),
+        configRouteComplete('PUT', '/history_cases/{id}', null, [CheckRoleUpdate], 'Histories', handlers.UpdateHistory(server)),
+        configRouteComplete('DELETE', '/history_cases/{id}', null, [CheckRoleUpdate], 'Histories', handlers.DeleteHistory(server)),
+        configRouteComplete('GET', '/history-export', null, [CheckRoleView], 'Histories', handlers.exportHistory(server)),
     ]
 }
