4034cc2f1f32ff4f96607bf6c4d3152e247b9adb
firmanjs <fimzhakim@gmail.com>
2020-11-17 14:56:54 +0700

    change user pre request

--- api/occupations/index.js
+++ api/occupations/index.js
@@ -1,8 +1,8 @@
 const Routes = require('./routes')
-const { routeOldNoPre } = require('../../helpers/routes')
+const { routeNoPreNew } = require('../../helpers/routes')
 
 const register = (server, options, next) => {
-  server.route(Routes(server, routeOldNoPre))
+  server.route(Routes(server, routeNoPreNew))
   return next()
 }
 
