2c0aa4193c66c47f1d0a575b35e789bab461246b
sandi <sandisunandar99@gmail.com>
2020-11-17 09:32:13 +0700

    fix ococupation

--- api/occupations/handlers.js
+++ api/occupations/handlers.js
@@ -12,11 +12,8 @@ const GetOccupationDetail = (server) => {
   return (id, reply) => {
     server.methods.services.occupations.getOccupationDetail(
       id,
-      (err, occupations) => {
-        if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-        return reply(
-          constructAreasResponse(occupations)
-        ).code(200)
+      (err, result) => {
+        replyJson(err, result, reply)
       }
     )
   }
