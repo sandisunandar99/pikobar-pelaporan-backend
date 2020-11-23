0b4315d20590737ccaa54e4dcf967caf5d9cb346
firmanjs <fimzhakim@gmail.com>
2020-11-23 12:14:54 +0700

    improvment(dashboard): ## OVERVIEW 1. improvment dashboard demographic 1. add age greather than 80

--- helpers/aggregate/demographicaggregate.js
+++ helpers/aggregate/demographicaggregate.js
@@ -2,7 +2,6 @@ const { filterStatus, filterNotGrouping } = require("./globalcondtion")
 const { sumFunc, sumBetweenFunc } = require("./func")
 const demographicCondition = (grouping, query, criteria) => {
   let query_state = filterStatus(query, criteria)
-  let status = filterNotGrouping(query, criteria)
   const paramsDemographic = {
     $group: {
       _id: grouping,
@@ -14,6 +13,11 @@ const demographicCondition = (grouping, query, criteria) => {
       six_nine: sumBetweenFunc(query_state, "$age", 5, 10),
       twenty_twenty_nine: sumBetweenFunc(query_state, "$age", 19, 30),
       thirty_thirty_nine: sumBetweenFunc(query_state, "$age", 29, 40),
+      forty_forty_nine: sumBetweenFunc(query_state, "$age", 39, 50),
+      fifty_fifty_nine: sumBetweenFunc(query_state, "$age", 49, 60),
+      sixty_sixty_nine: sumBetweenFunc(query_state, "$age", 59, 70),
+      seventy_seventy_nine: sumBetweenFunc(query_state, "$age", 69, 80),
+      greater_eighty: sumBetweenFunc(query_state, "$age", 80, 150)
     }
   }
 
