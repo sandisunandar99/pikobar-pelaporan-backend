06aafefa0e08d97b72ca211bf54873f4cbb61ce4
firmanjs <fimzhakim@gmail.com>
2020-11-26 15:01:18 +0700

    remove unused unit endpoint

--- api/unit/handlers.js
+++ api/unit/handlers.js
@@ -1,31 +1,23 @@
 const { replyHelper, successResponse } = require('../helpers');
 
+const unitService = (server, name, method, req, request, reply) => {
+  server.methods.services[name][method](req, (err, result) => {
+    if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
+      return reply(
+        successResponse(result, request)
+      ).code(200)
+    }
+  )
+}
+
 module.exports = (server) => {
 
   return {
-    /**
-     * GET /api/unit
-     * @param {*} request
-     * @param {*} reply
-     */
     async getUnit(request, reply) {
-      server.methods.services.unit.read(request.query, (err, result) => {
-        if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-        return reply(
-          successResponse(result, request)
-        ).code(200)
-      }
-      )
+      unitService(server, "unit", "read", request.query, request, reply)
     },
     async listUnitById(request, reply) {
-      server.methods.services.unit.readbyid(request.params.id,
-        (err, result) => {
-          if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-          return reply(
-            successResponse(result, request)
-          ).code(200)
-        }
-      )
-    },
+      unitService(server, "unit", "readbyid", request.params.id, request, reply)
+    }
   };
 }
