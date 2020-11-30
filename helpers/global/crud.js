c96c383642377147a251a81628c7ec2583f12fb0
firmanjs <fimzhakim@gmail.com>
2020-11-30 10:30:02 +0700

    with return

--- helpers/global/crud.js
+++ helpers/global/crud.js
@@ -1,26 +1,18 @@
 const ObjectId = require('mongodb').ObjectID
 
 const findGlobal = async (Schema, id_case, select) => {
-  try {
-    await Schema.find({ _id: id_case })
-      .select([select])
-      .sort({ updatedAt: -1 })
-  } catch (error) {
-    error
-  }
+  return await Schema.find({ _id: id_case })
+    .select([select])
+    .sort({ updatedAt: -1 })
 }
 
 const deleteGlobal = async (Schema, column, id) => {
-  try {
-    const columnId = `${column}._id`
-    const condition = {
-      [columnId]: ObjectId(id)
-    }
-    const pull = { [column]: { _id: ObjectId(id) } }
-    await Schema.updateOne(condition, { $pull: pull })
-  } catch (error) {
-    error
+  const columnId = `${column}._id`
+  const condition = {
+    [columnId]: ObjectId(id)
   }
+  const pull = { [column]: { _id: ObjectId(id) } }
+  return await Schema.updateOne(condition, { $pull: pull })
 }
 
 module.exports = {
