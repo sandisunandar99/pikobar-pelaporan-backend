21a578638bd58bb01ab1c1af79f591f360fdc943
firmanjs <fimzhakim@gmail.com>
2020-12-04 10:01:26 +0700

    history travel route

--- api/local_transmission/routes.js
+++ api/local_transmission/routes.js
@@ -1,12 +1,13 @@
 module.exports = (server, route) => {
   const service = 'local_transmission'
+  const path = '/local-transmission/'
   const CheckRoleView = require('../users/route_prerequesites').CheckRoleView(server)
   const CheckRole = require('../users/route_prerequesites').CheckRoleDelete(server)
 
   return [
-    route(server, 'POST', `/${service}/{id_case}`, service, CheckRole, 'createLocalTransmission'),
-    route(server, 'GET', `/${service}/{id_case}`, service, CheckRoleView, 'getLocalTransmission'),
-    route(server, 'PUT', `/${service}/{id_local_transmission}`, service, CheckRole, 'updateLocalTransmission'),
-    route(server, 'DELETE', `/${service}/{id_local_transmission}`, service, CheckRole, 'deleteLocalTransmission')
+    route(server, 'POST', `${path}{id_case}`, service, CheckRole, 'createLocalTransmission'),
+    route(server, 'GET', `${path}{id_case}`, service, CheckRoleView, 'getLocalTransmission'),
+    route(server, 'PUT', `${path}{id_local_transmission}`, service, CheckRole, 'updateLocalTransmission'),
+    route(server, 'DELETE', `${path}{id_local_transmission}`, service, CheckRole, 'deleteLocalTransmission')
   ]
 }
