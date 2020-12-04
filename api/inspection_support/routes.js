115258e49e0e7d0ff18f814d478b0fdb58a4562f
firmanjs <fimzhakim@gmail.com>
2020-12-04 10:06:33 +0700

    inspection support route

--- api/inspection_support/routes.js
+++ api/inspection_support/routes.js
@@ -1,52 +1,13 @@
-module.exports = (server) => {
-  const handlers = require('./handlers')
-  const CheckRoleView = require('../users/route_prerequesites').CheckRoleView(server);
-  const CheckRole = require('../users/route_prerequesites').CheckRoleCreate(server)
+module.exports = (server, route) => {
+  const service = 'inspection_support'
+  const path = '/inspection-support/'
+  const CheckRoleView = require('../users/route_prerequesites').CheckRoleView(server)
+  const CheckRole = require('../users/route_prerequesites').CheckRoleDelete(server)
 
   return [
-    {
-      method: 'POST',
-      path: '/inspection-support/{id_case}',
-      config: {
-        auth: 'jwt',
-        description: 'create inspection-support',
-        tags: ['api', 'inspection-support'],
-        pre: [ CheckRole ]
-      },
-      handler: handlers.createInspectionSupport(server)
-    },
-    {
-      method: 'GET',
-      path: '/inspection-support/{id_case}',
-      config: {
-        auth: 'jwt',
-        description: 'show list inspection-support',
-        tags: ['api', 'inspection-support'],
-        pre: [ CheckRoleView ]
-      },
-      handler:  handlers.getInspectionSupport(server)
-    },
-    {
-      method: 'PUT',
-      path: '/inspection-support/{id_inspection_support}',
-      config: {
-        auth: 'jwt',
-        description: 'update inspection-support',
-        tags: ['api', 'inspection-support'],
-        pre: [ CheckRole ],
-      },
-      handler:  handlers.updateInspectionSupport(server)
-    },
-    {
-      method: 'DELETE',
-      path: '/inspection-support/{id_inspection_support}',
-      config: {
-        auth: 'jwt',
-        description: 'delete inspection-support',
-        tags: ['api', 'inspection-support'],
-        pre: [ CheckRole ],
-      },
-      handler: handlers.deleteInspectionSupport(server)
-    }
+    route(server, 'POST', `${path}{id_case}`, service, CheckRole, 'createInspectionSupport'),
+    route(server, 'GET', `${path}{id_case}`, service, CheckRoleView, 'getInspectionSupport'),
+    route(server, 'PUT', `${path}{id_inspection_support}`, service, CheckRole, 'updateInspectionSupport'),
+    route(server, 'DELETE', `${path}{id_inspection_support}`, service, CheckRole, 'deleteInspectionSupport')
   ]
 }
