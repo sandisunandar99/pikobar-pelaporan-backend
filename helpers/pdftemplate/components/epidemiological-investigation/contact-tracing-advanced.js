1c210945e23864736efd1baff99e1d3626f0db4a
Khi Hady Sucahyo <khihady.ks@gmail.com>
2020-12-07 11:23:01 +0700

    refactor per-form contacttracing

--- helpers/pdftemplate/components/epidemiological-investigation/contact-tracing-advanced.js
+++ helpers/pdftemplate/components/epidemiological-investigation/contact-tracing-advanced.js
@@ -4,81 +4,193 @@ const {
 
 const moment = require('moment')
 
-const render = (data) => {
+const isTrue = (value) => {
+  return value ? '√' : '  '
+}
 
-  const officer = data.close_contact_health_worker
+const isFalse = (value) => {
+  return !value ? '√' : '  '
+}
+
+const formattedDate = (d) => {
+  return d ? moment(d).format('YYYY/MM/DD') : '-'
+}
+
+const confirmedContact = (data, criterias) => {
+  let res = false
+  let records = data
+  if (!records) {
+    return false
+  }
+
+  records = records.closeContacts
+  if (!records || !records.length) {
+    return false
+  }
+
+  records.forEach(v => {
+    if (criterias.includes(v.status)) {
+      res = true
+    }
+  })
+
+  return res
+}
+
+const buildConfirmedContact = (data, criterias) => {
+  let res = []
+
+  let records = []
+  if (data && data.closeContacts) {
+    records = data.closeContacts
+  }
+
+  for (i in records) {
+    const rec = records[i]
+    if (!criterias.includes(rec.status)) continue
+
+    res.push([
+        { alignment: 'left', text: rec.name || '-' },
+        { alignment: 'left', text: rec.address_street || '-' },
+        { alignment: 'center', text: rec.close_contact_relation || '-' },
+        { alignment: 'center', text: formattedDate(rec.first_contact_date) },
+        { alignment: 'center', text: formattedDate(rec.last_contact_date) },
+    ])
+  }
+
+  if (!res.length) {
+    res.push([
+      { alignment: 'center', text: '- Tidak ada riwayat -', colSpan: 5 },{},{},{},{},
+    ])
+  }
+
+  return res
+}
+
+const compConfirmedContact = data => {
+  return [
+    [
+      {
+        text: 'D FAKTOR KONTAK/PAPARAN (lanjutan)',
+        style: 'tableHeader',
+        colSpan: 5,
+        alignment: 'left'
+      },{},{},{},{}
+    ],
+    [
+      {
+        // border: ['black', '','','black'],
+        text: 'Dalam 14 hari sebelum sakit, apakah memiliki kontak erat dengan kasus konfirmasi dan probable COVID-19?',
+        colSpan: 2,
+        alignment: 'left'
+      },
+      {},
+      {
+        // border: ['', '','black','black'],
+        text: `: [${confirmedContact(data, [CRITERIA.CONF]) ? '√' : '  ' }] Ya   [${!confirmedContact(data, [CRITERIA.CONF]) ? '√' : '  ' }] Tdk  [  ] Tdk Tahu`,
+        colSpan: 3,
+        alignment: 'left'
+      },{},{}
+    ],
+    [
+      { text: 'Nama', style: 'tableColumnSubHeader' },
+      { text: 'Alamat', style: 'tableColumnSubHeader'  },
+      { text: 'Hubungan', style: 'tableColumnSubHeader'  },
+      { text: 'Tgl kontak pertama', style: 'tableColumnSubHeader'  },
+      { text: 'Tgl kontak terakhir', style: 'tableColumnSubHeader'  },
+    ],
+    ...buildConfirmedContact(data, [CRITERIA.CONF]),
+  ]
+}
+
+const compIspaGroup = data => {
   const ispaGroup = data.close_contact_heavy_ispa_group
-  const aerosol = data.close_contact_performing_aerosol_procedures
-  const aerosolSubject = data.close_contact_performing_aerosol
+  return [
+    [
+      {
+        text: 'Apakah pasien termasuk cluster ISPA berat (demam dan  pneumonia membutuhkan perawatan Rumah Sakit) yang tidak diketahui peyebabnya dimana kasus COVID-19 diperiksa?',
+        colSpan: 2,
+        alignment: 'left'
+      },
+      {},
+      {
+        text: `: [${isTrue(ispaGroup)}] Ya   [${isFalse(ispaGroup)}] Tdk  [  ] Tdk Tahu`,
+        colSpan: 3,
+        alignment: 'left'
+      },{},{}
+    ],
+  ]
+}
+
+const compHealthWorker = data => {
+  const officer = data.close_contact_health_worker
   const protectionTools = data.apd_use ? data.apd_use.map(v => v.toLowerCase()) : []
 
   const isProtected = (value) => {
     return protectionTools.includes(value) ? '√' : '  '
   }
+  return [
+    [
+      {
+        text: 'Apakah pasien seorang petugas kesehatan?',
+        colSpan: 2,
+        alignment: 'left'
+      },
+      {},
+      {
+        text: `: [${isTrue(officer)}] Ya   [${isFalse(officer)}] Tdk  [  ] Tdk Tahu`,
+        colSpan: 3,
+        alignment: 'left'
+      },{},{}
+    ],
+    [
+      {
+        text: 'Jika iya Palat pelindung diri (APD) apa yang digunakan?',
+        colSpan: 2,
+        alignment: 'left'
+      },
+      {},
+      {
+        text: `: [${isProtected('gown')}] Gown   [${isProtected('masker bedah')}] Masker Medis   [${isProtected('sarung tangan')}] Sarung tangan
+          : [${isProtected('masker n95 standar ffp3')}] Masker NI0SH-N95, AN EU STANDARD FFP2
+          : [${isProtected('ffp3')}] FFP3
+          : [${isProtected('kacamata pelindung goggle')}] Kacamata pelindung (goggle)
+          : [${isProtected('tidak sama sekali')}] Tidak memakai APD`,
+        colSpan: 3,
+        alignment: 'left'
+      },{},{}
+    ],
+  ]
+}
 
-  const isTrue = (value) => {
-    return value ? '√' : '  '
-  }
-
-  const isFalse = (value) => {
-    return !value ? '√' : '  '
-  }
-
-  const formattedDate = (d) => {
-    return d ? moment(d).format('YYYY/MM/DD') : '-'
-  }
-
-  const confirmedContact = (criterias) => {
-    let res = false
-    let records = data
-    if (!records) {
-      return false
-    }
-
-    records = records.closeContacts
-    if (!records || !records.length) {
-      return false
-    }
-
-    records.forEach(v => {
-      if (criterias.includes(v.status)) {
-        res = true
-      }
-    })
-
-    return res
-  }
-
-  const buildConfirmedContact = (criterias) => {
-    let res = []
-
-    let records = []
-    if (data && data.closeContacts) {
-      records = data.closeContacts
-    }
-
-    for (i in records) {
-      const rec = records[i]
-      if (!criterias.includes(rec.status)) continue
-
-      res.push([
-          { alignment: 'left', text: rec.name || '-' },
-          { alignment: 'left', text: rec.address_street || '-' },
-          { alignment: 'center', text: rec.close_contact_relation || '-' },
-          { alignment: 'center', text: formattedDate(rec.first_contact_date) },
-          { alignment: 'center', text: formattedDate(rec.last_contact_date) },
-      ])
-    }
-
-    if (!res.length) {
-      res.push([
-        { alignment: 'center', text: '- Tidak ada riwayat -', colSpan: 5 },{},{},{},{},
-      ])
-    }
-
-    return res
-  }
-
+const compAerosolProcedure = data => {
+  const aerosol = data.close_contact_performing_aerosol_procedures
+  const aerosolSubject = data.close_contact_performing_aerosol
+  return [
+    [
+      {
+        text: 'Apakah melakukan prosedur yang menimbulkan aerosol?',
+        colSpan: 2,
+        alignment: 'left'
+      },
+      {},
+      {
+        text: `: [${isTrue(aerosol)}] Ya   [${isFalse(aerosol)}] Tdk, Sebutkan: ${aerosolSubject || '-'}`,
+        colSpan: 3,
+        alignment: 'left'
+      },{},{}
+    ],
+    [
+      {
+        text: 'Lain-lain, sebutkan\n.\n.',
+        colSpan: 5,
+        alignment: 'left'
+      },
+      {},{},{}
+    ]
+  ]
+}
+const render = (data) => {
   return [
     {
       // margin: [0, -15, 0, 0],
@@ -89,101 +201,10 @@ const render = (data) => {
         widths: [105, 157, 81, 65, 65],
         headerRows: 1,
         body: [
-          [
-            {
-              text: 'D FAKTOR KONTAK/PAPARAN (lanjutan)',
-              style: 'tableHeader',
-              colSpan: 5,
-              alignment: 'left'
-            },{},{},{},{}
-          ],
-          [
-            {
-              // border: ['black', '','','black'],
-              text: 'Dalam 14 hari sebelum sakit, apakah memiliki kontak erat dengan kasus konfirmasi dan probable COVID-19?',
-              colSpan: 2,
-              alignment: 'left'
-            },
-            {},
-            {
-              // border: ['', '','black','black'],
-              text: `: [${confirmedContact([CRITERIA.CONF]) ? '√' : '  ' }] Ya   [${!confirmedContact([CRITERIA.CONF]) ? '√' : '  ' }] Tdk  [  ] Tdk Tahu`,
-              colSpan: 3,
-              alignment: 'left'
-            },{},{}
-          ],
-          [
-            { text: 'Nama', style: 'tableColumnSubHeader' },
-            { text: 'Alamat', style: 'tableColumnSubHeader'  },
-            { text: 'Hubungan', style: 'tableColumnSubHeader'  },
-            { text: 'Tgl kontak pertama', style: 'tableColumnSubHeader'  },
-            { text: 'Tgl kontak terakhir', style: 'tableColumnSubHeader'  },
-          ],
-          ...buildConfirmedContact([CRITERIA.CONF]),
-          [
-            {
-              text: 'Apakah pasien termasuk cluster ISPA berat (demam dan  pneumonia membutuhkan perawatan Rumah Sakit) yang tidak diketahui peyebabnya dimana kasus COVID-19 diperiksa?',
-              colSpan: 2,
-              alignment: 'left'
-            },
-            {},
-            {
-              text: `: [${isTrue(ispaGroup)}] Ya   [${isFalse(ispaGroup)}] Tdk  [  ] Tdk Tahu`,
-              colSpan: 3,
-              alignment: 'left'
-            },{},{}
-          ],
-          [
-            {
-              text: 'Apakah pasien seorang petugas kesehatan?',
-              colSpan: 2,
-              alignment: 'left'
-            },
-            {},
-            {
-              text: `: [${isTrue(officer)}] Ya   [${isFalse(officer)}] Tdk  [  ] Tdk Tahu`,
-              colSpan: 3,
-              alignment: 'left'
-            },{},{}
-          ],
-          [
-            {
-              text: 'Jika iya Palat pelindung diri (APD) apa yang digunakan?',
-              colSpan: 2,
-              alignment: 'left'
-            },
-            {},
-            {
-              text: `: [${isProtected('gown')}] Gown   [${isProtected('masker bedah')}] Masker Medis   [${isProtected('sarung tangan')}] Sarung tangan
-                : [${isProtected('masker n95 standar ffp3')}] Masker NI0SH-N95, AN EU STANDARD FFP2
-                : [${isProtected('ffp3')}] FFP3
-                : [${isProtected('kacamata pelindung goggle')}] Kacamata pelindung (goggle)
-                : [${isProtected('tidak sama sekali')}] Tidak memakai APD`,
-              colSpan: 3,
-              alignment: 'left'
-            },{},{}
-          ],
-          [
-            {
-              text: 'Apakah melakukan prosedur yang menimbulkan aerosol?',
-              colSpan: 2,
-              alignment: 'left'
-            },
-            {},
-            {
-              text: `: [${isTrue(aerosol)}] Ya   [${isFalse(aerosol)}] Tdk, Sebutkan: ${aerosolSubject || '-'}`,
-              colSpan: 3,
-              alignment: 'left'
-            },{},{}
-          ],
-          [
-            {
-              text: 'Lain-lain, sebutkan\n.\n.',
-              colSpan: 5,
-              alignment: 'left'
-            },
-            {},{},{}
-          ]
+          ...compConfirmedContact(data),
+          ...compIspaGroup(data),
+          ...compHealthWorker(data),
+          ...compAerosolProcedure(data),
         ],
       }
     },
--- helpers/pdftemplate/components/epidemiological-investigation/contact-tracing.js
+++ helpers/pdftemplate/components/epidemiological-investigation/contact-tracing.js
@@ -1,273 +1,259 @@
 const moment = require('moment')
-const {
-  CRITERIA
-} = require('../../../constant')
+const { CRITERIA } = require('../../../constant')
+
+const isTrue = (data, value) => data[value] ? '√' : '  '
+const isFalse = (data, value) =>  !data[value] ? '√' : '  '
+const formattedDate = d => d ? moment(d).format('YYYY/MM/DD') : '-'
+
+const buildTravelPlaces = (data, place, type) => {
+  let res = [], exception
+
+  const records = data[place] || []
+
+  switch (type) {
+    case 'domestic': exception = 'Dari Luar Kota'; break;
+    default: exception = 'Dari Luar Negeri';
+  }
+
+  for (i in records) {
+    const rec = records[i]
+
+    if (rec.travelling_type === exception) continue
+
+    res.push([
+        { alignment: 'center', text: rec.travelling_visited || '-' },
+        { alignment: 'center', text: rec.travelling_city || '-' },
+        { alignment: 'center', text: formattedDate(rec.travelling_date) },
+        { alignment: 'center', text: formattedDate(rec.travelling_arrive) },
+    ])
+  }
+
+  if (!res.length) {
+    res.push([{ alignment: 'center', text: '- Tidak ada riwayat -', colSpan: 4 },{},{},{}])
+  }
+
+  return res
+}
+
+const buildResidences = (data) => {
+  let res = []
+
+  const records = data.visited_local_area || []
+
+  for (i in records) {
+    const rec = records[i]
+    res.push([
+        { alignment: 'center', colSpan: 2, text: rec.visited_local_area_province || '-' },{},
+        { alignment: 'center', colSpan: 2, text: rec.visited_local_area_city || '-' },{}
+    ])
+  }
+
+  if (!res.length) {
+    res.push([{ alignment: 'center', text: '- Tidak ada riwayat -', colSpan: 4 },{},{},{}])
+  }
+
+  return res
+}
+
+const travelType = (data, type) => {
+  let res = false
+  let records = data
+  if (!records) return false
+
+  records = records.travelling_history
+  if (!records || !records.length) return false
+
+  switch (type) {
+    case 'INTL': type = 'Dari Luar Negeri'; break;
+    default: type = 'Dari Luar Kota';
+  }
+
+  records.forEach(v => { if (v.travelling_type === type) { res = true } })
+  return res
+}
+
+const suspectContact = (data, criterias) => {
+  let res = false
+  let records = data
+  if (!records) return false
+
+  records = records.closeContacts
+  if (!records || !records.length) return false
+
+  records.forEach(v => { if (criterias.includes(v.status)) { res = true } })
+
+  return res
+}
+
+
+const buildSuspectContact = (data, criterias) => {
+  let res = []
+
+  let records = []
+  if (data && data.closeContacts) {
+    records = data.closeContacts
+  }
+
+  for (i in records) {
+    const rec = records[i]
+    if (!criterias.includes(rec.status)) continue
+
+    res.push([
+        { alignment: 'left', text: rec.name || '-' },
+        { alignment: 'left', text: rec.address_street || '-' },
+        { alignment: 'center', text: rec.relation || '-' },
+        { alignment: 'center', text: formattedDate(rec.first_contact_date) },
+        { alignment: 'center', text: formattedDate(rec.last_contact_date) },
+    ])
+  }
+
+  if (!res.length) {
+    res.push([{ alignment: 'center', text: '- Tidak ada riwayat -', colSpan: 5 },{},{},{},{}])
+  }
+
+  return res
+}
+
+const compTravelingHis = data => {
+  return [
+    [
+      {
+        text: 'D FAKTOR RIWAYAT PERJALANAN',
+        style: 'tableHeader',
+        colSpan: 4,
+        alignment: 'left'
+      },{},{},{}
+    ],
+    [
+      {
+        border: ['black', 'black','','black'],
+        text: 'Dalam 14 hari sebelum sakit, apakah memiliki riwayat Perjalanan ke luar negeri?',
+        colSpan: 2,
+        alignment: 'left'
+      },{},
+      {
+        border: ['', 'black','black','black'],
+        text: `: [${travelType(data, 'INTL') ? '√' : '  ' }] Ya   [${!travelType(data, 'INTL') ? '√' : '  ' }] Tdk  [  ] Tdk Tahu`,
+        colSpan: 2,
+        alignment: 'left'
+      },{}
+    ],
+    [
+      { text: 'Negara', style: 'tableColumnSubHeader' },
+      { text: 'Kota', style: 'tableColumnSubHeader'  },
+      { text: 'Tgl Perjalanan', style: 'tableColumnSubHeader'  },
+      { text: 'Tgl tiba di Indonesia', style: 'tableColumnSubHeader'  },
+    ],
+    ...buildTravelPlaces(data, 'travelling_history', 'international'),
+  ]
+}
+
+const compTravePlaces = (data) => {
+  return [
+    [
+      {
+        border: ['black', 'black','','black'],
+        text: 'Dalam 14 hari sebelum sakit, apakah memiliki riwayat Perjalanan ke area transmisi lokal?',
+        colSpan: 2,
+        alignment: 'left'
+      },
+      {},
+      {
+        border: ['', 'black','black','black'],
+        text: `: [${travelType(data, 'DOM') ? '√' : '  ' }] Ya   [${!travelType(data, 'DOM') ? '√' : '  ' }] Tdk  [  ] Tdk Tahu`,
+        colSpan: 2,
+        alignment: 'left'
+      },{}
+    ],
+    [
+      { text: 'Provinsi', style: 'tableColumnSubHeader' },
+      { text: 'Kota', style: 'tableColumnSubHeader'  },
+      { text: 'Tgl Perjalanan', style: 'tableColumnSubHeader'  },
+      { text: 'Tgl tiba di tempat', style: 'tableColumnSubHeader'  },
+    ],
+    ...buildTravelPlaces(data, 'travelling_history', 'domestic'),
+  ]
+}
+
+const compLocalTransmission = data => {
+  return [
+    [
+      {
+        border: ['black', 'black','','black'],
+        text: 'Dalam 14 hari sebelum sakit, apakah memiliki riwayat tinggal ke area transmisi lokal?',
+        colSpan: 2,
+        alignment: 'left'
+      },
+      {},
+      {
+        border: ['', 'black','black','black'],
+        text: `: [${isTrue(data, 'visited_local_area_before_sick_14_days')}] Ya   [${isFalse(data, 'visited_local_area_before_sick_14_days')}] Tdk  [  ] Tdk Tahu`,
+        colSpan: 2,
+        alignment: 'left'
+      },{}
+    ],
+    [
+      { text: 'Provinsi', colSpan: 2, style: 'tableColumnSubHeader' },
+      {},
+      { text: 'Kota', colSpan: 2, style: 'tableColumnSubHeader'  },
+      {},
+    ],
+    ...buildResidences(data)
+  ]
+}
+
+const compSuspectContact = data => {
+  return [
+    [
+      {
+        border: ['black', 'black','','black'],
+        text: 'Dalam 14 hari sebelum sakit, apakah memiliki kontak dengan kasus suspek/probable COVID-19 ?',
+        colSpan: 2,
+        alignment: 'left'
+      },
+      {},
+      {
+        border: ['', 'black','black','black'],
+        text: '',
+        text: `: [${suspectContact(data, [CRITERIA.SUS, CRITERIA.PROB]) ? '√' : '  ' }] Ya   [${!suspectContact(data, [CRITERIA.SUS, CRITERIA.PROB]) ? '√' : '  ' }] Tdk  [  ] Tdk Tahu`,
+        colSpan: 3,
+        alignment: 'left'
+      },{},{}
+    ],
+    [
+      { text: 'Nama', style: 'tableColumnSubHeader' },
+      { text: 'Alamat', style: 'tableColumnSubHeader'  },
+      { text: 'Hubungan', style: 'tableColumnSubHeader'  },
+      { text: 'Tgl Kontak Pertama', style: 'tableColumnSubHeader'  },
+      { text: 'Tgl Kontak Terakhir', style: 'tableColumnSubHeader'  },
+    ],
+    ...buildSuspectContact(data, [CRITERIA.SUS, CRITERIA.PROB]),
+  ]
+}
+
+const tableHeader = { style: 'tableClinical', color: '#444' }
+
 const render = (data) => {
-
-  const isTrue = (value) => {
-    return data[value] ? '√' : '  '
-  }
-
-  const isFalse = (value) => {
-    return !data[value] ? '√' : '  '
-  }
-
-  const formattedDate = (d) => {
-    return d ? moment(d).format('YYYY/MM/DD') : '-'
-  }
-
-  const buildTravelPlaces = (place, type) => {
-    let res = []
-
-    const records = data[place] || []
-
-    const exception = type === 'domestic'
-      ? 'Dari Luar Negeri'
-      : 'Dari Luar Kota'
-
-    for (i in records) {
-      const rec = records[i]
-
-      if (rec.travelling_type === exception) continue
-
-      res.push([
-          { alignment: 'center', text: rec.travelling_visited || '-' },
-          { alignment: 'center', text: rec.travelling_city || '-' },
-          { alignment: 'center', text: formattedDate(rec.travelling_date) },
-          { alignment: 'center', text: formattedDate(rec.travelling_arrive) },
-      ])
-    }
-
-    if (!res.length) {
-      res.push([
-        { alignment: 'center', text: '- Tidak ada riwayat -', colSpan: 4 },{},{},{},
-      ])
-    }
-
-    return res
-  }
-
-  const buildResidences = () => {
-    let res = []
-
-    const records = data.visited_local_area || []
-
-    for (i in records) {
-      const rec = records[i]
-      res.push([
-          { alignment: 'center', colSpan: 2, text: rec.visited_local_area_province || '-' },{},
-          { alignment: 'center', colSpan: 2, text: rec.visited_local_area_city || '-' },{}
-      ])
-    }
-
-    if (!res.length) {
-      res.push([
-        { alignment: 'center', text: '- Tidak ada riwayat -', colSpan: 4 },{},{},{},
-      ])
-    }
-
-    return res
-  }
-
-  const travelType = (type) => {
-    let res = false
-    let records = data
-    if (!records) {
-      return false
-    }
-
-    records = records.travelling_history
-    if (!records || !records.length) {
-      return false
-    }
-
-    type = type === 'INTL'
-      ? 'Dari Luar Negeri'
-      : 'Dari Luar Kota'
-
-    records.forEach(v => {
-      if (v.travelling_type === type) {
-        res = true
-      }
-    })
-
-    return res
-  }
-
-  const suspectContact = (criterias) => {
-    let res = false
-    let records = data
-    if (!records) {
-      return false
-    }
-
-    records = records.closeContacts
-    if (!records || !records.length) {
-      return false
-    }
-
-    records.forEach(v => {
-      if (criterias.includes(v.status)) {
-        res = true
-      }
-    })
-
-    return res
-  }
-
-  const buildSuspectContact = (criterias) => {
-    let res = []
-
-    let records = []
-    if (data && data.closeContacts) {
-      records = data.closeContacts
-    }
-
-    for (i in records) {
-      const rec = records[i]
-      if (!criterias.includes(rec.status)) continue
-
-      res.push([
-          { alignment: 'left', text: rec.name || '-' },
-          { alignment: 'left', text: rec.address_street || '-' },
-          { alignment: 'center', text: rec.relation || '-' },
-          { alignment: 'center', text: formattedDate(rec.first_contact_date) },
-          { alignment: 'center', text: formattedDate(rec.last_contact_date) },
-      ])
-    }
-
-    if (!res.length) {
-      res.push([
-        { alignment: 'center', text: '- Tidak ada riwayat -', colSpan: 5 },{},{},{},{},
-      ])
-    }
-
-    return res
-  }
 
   return [
     {
-      style: 'tableClinical',
-      color: '#444',
+      ...tableHeader,
       table: {
         widths: [145, 115, 110, 112],
         headerRows: 1,
-        body: [
-          [
-            {
-              text: 'D FAKTOR RIWAYAT PERJALANAN',
-              style: 'tableHeader',
-              colSpan: 4,
-              alignment: 'left'
-            },{},{},{}
-          ],
-          [
-            {
-              border: ['black', 'black','','black'],
-              text: 'Dalam 14 hari sebelum sakit, apakah memiliki riwayat Perjalanan ke luar negeri?',
-              colSpan: 2,
-              alignment: 'left'
-            },
-            {},
-            {
-              border: ['', 'black','black','black'],
-              text: `: [${travelType('INTL') ? '√' : '  ' }] Ya   [${!travelType('INTL') ? '√' : '  ' }] Tdk  [  ] Tdk Tahu`,
-              colSpan: 2,
-              alignment: 'left'
-            },{}
-          ],
-          [
-            { text: 'Negara', style: 'tableColumnSubHeader' },
-            { text: 'Kota', style: 'tableColumnSubHeader'  },
-            { text: 'Tgl Perjalanan', style: 'tableColumnSubHeader'  },
-            { text: 'Tgl tiba di Indonesia', style: 'tableColumnSubHeader'  },
-          ],
-          ...buildTravelPlaces('travelling_history', 'international'),
-          [
-            {
-              border: ['black', 'black','','black'],
-              text: 'Dalam 14 hari sebelum sakit, apakah memiliki riwayat Perjalanan ke area transmisi lokal?',
-              colSpan: 2,
-              alignment: 'left'
-            },
-            {},
-            {
-              border: ['', 'black','black','black'],
-              text: `: [${travelType('DOM') ? '√' : '  ' }] Ya   [${!travelType('DOM') ? '√' : '  ' }] Tdk  [  ] Tdk Tahu`,
-              colSpan: 2,
-              alignment: 'left'
-            },{}
-          ],
-          [
-            { text: 'Provinsi', style: 'tableColumnSubHeader' },
-            { text: 'Kota', style: 'tableColumnSubHeader'  },
-            { text: 'Tgl Perjalanan', style: 'tableColumnSubHeader'  },
-            { text: 'Tgl tiba di tempat', style: 'tableColumnSubHeader'  },
-          ],
-          ...buildTravelPlaces('travelling_history', 'domestic'),
-          [
-            {
-              border: ['black', 'black','','black'],
-              text: 'Dalam 14 hari sebelum sakit, apakah memiliki riwayat tinggal ke area transmisi lokal?',
-              colSpan: 2,
-              alignment: 'left'
-            },
-            {},
-            {
-              border: ['', 'black','black','black'],
-              text: `: [${isTrue('visited_local_area_before_sick_14_days')}] Ya   [${isFalse('visited_local_area_before_sick_14_days')}] Tdk  [  ] Tdk Tahu`,
-              colSpan: 2,
-              alignment: 'left'
-            },{}
-          ],
-          [
-            { text: 'Provinsi', colSpan: 2, style: 'tableColumnSubHeader' },
-            {},
-            { text: 'Kota', colSpan: 2, style: 'tableColumnSubHeader'  },
-            {},
-          ],
-          ...buildResidences()
-        ],
+        body: [ ...compTravelingHis(data), ...compTravePlaces(data), ...compLocalTransmission(data) ],
       }
     },
     {
+      ...tableHeader,
       margin: [0, -15, 0, 0],
-      style: 'tableClinical',
-      color: '#444',
       table: {
         widths: [105, 157, 81, 65, 65],
         headerRows: 1,
-        body: [
-          [
-            {
-              border: ['black', 'black','','black'],
-              text: 'Dalam 14 hari sebelum sakit, apakah memiliki kontak dengan kasus suspek/probable COVID-19 ?',
-              colSpan: 2,
-              alignment: 'left'
-            },
-            {},
-            {
-              border: ['', 'black','black','black'],
-              text: '',
-              text: `: [${suspectContact([CRITERIA.SUS, CRITERIA.PROB]) ? '√' : '  ' }] Ya   [${!suspectContact([CRITERIA.SUS, CRITERIA.PROB]) ? '√' : '  ' }] Tdk  [  ] Tdk Tahu`,
-              colSpan: 3,
-              alignment: 'left'
-            },{},{}
-          ],
-          [
-            { text: 'Nama', style: 'tableColumnSubHeader' },
-            { text: 'Alamat', style: 'tableColumnSubHeader'  },
-            { text: 'Hubungan', style: 'tableColumnSubHeader'  },
-            { text: 'Tgl Kontak Pertama', style: 'tableColumnSubHeader'  },
-            { text: 'Tgl Kontak Terakhir', style: 'tableColumnSubHeader'  },
-          ],
-          ...buildSuspectContact([CRITERIA.SUS, CRITERIA.PROB]),
-        ]
+        body: [ ...compSuspectContact(data) ]
       }
     }
   ]
 }
 
-module.exports = {
-  render
-}
+module.exports = { render }
