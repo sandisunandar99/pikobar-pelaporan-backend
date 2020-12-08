5d832f38fbce0f143479c2f982f805387a84466a
firmanjs <fimzhakim@gmail.com>
2020-12-08 13:11:41 +0700

    improvement(daily-report): ## OVERVIEW 1. filter by faskes 1. list by faskes 1. export by faskes
    #731

--- api/reports/routes.js
+++ api/reports/routes.js
@@ -1,6 +1,6 @@
 module.exports = (server, route) => {
   return [
-    route(server, 'GET', '/reports/daily-report', 'reports', 'DailyReport'),
-    route(server, 'GET', '/reports/daily-report-xls', 'reports', 'DailyReportXls'),
+    route(server, 'GET', '/reports/daily-report', 'reports', 'dailyReport'),
+    route(server, 'GET', '/reports/daily-report-xls', 'reports', 'dailyReportXls'),
   ]
 }
