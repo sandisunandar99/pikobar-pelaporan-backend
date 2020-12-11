e4a0c1cbe2b226d8f5d0c28bb126d7e9a5604aed
sandi <sandisunandar99@gmail.com>
2020-12-11 08:52:27 +0700

    add index

---
+++ helpers/rdt/sheet/index.js
@@ -0,0 +1,71 @@
+const dir = './upload/'
+const xlsx = require('node-xlsx')
+const mongoose = require('mongoose')
+const config = require('../sheet/config.json')
+const {createPayload} = require('./helper')
+
+const extractToJson = async (request) => {
+    //  generate unique import batch id (debug purpose)
+    const uniqueBatchId = require('uuid').v4()
+
+    const uploaded = await handleFileUpload(request.payload.file)
+
+    const dataSheet = (await xlsx.parse(dir + uploaded.filename))[0]['data']
+
+    dataSheet.splice(0, config.start_row)
+
+    let payload = []
+
+    for (let i in dataSheet) {
+      const data = dataSheet[i]
+
+      let obj = await createPayload(data, uniqueBatchId)
+      console.log(obj);
+
+      // Object.keys(obj).map(k => {
+      //   if (obj[k] && obj[k].trim) {
+      //     obj[k] = obj[k].trim()
+      //   }
+      // })
+
+      // payload.push(obj)
+    }
+
+    // handleFileUnlink(dir + uploaded.filename)
+    // return payload
+}
+
+const handleFileUpload = file => {
+  const fs = require('fs')
+  const dir = './upload/'
+
+  return new Promise((resolve, reject) => {
+    var filename = new Date().getTime() + '_' + file.hapi.filename.replace(' ', '')
+    const data = file._data
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir)
+    }
+
+    fs.writeFile(dir + filename, data, err => {
+      if (err) {
+        return callback(err, null)
+      }
+      resolve({ filename: filename })
+    })
+  })
+}
+
+const handleFileUnlink = file => {
+  const fs = require('fs')
+  return fs.unlink(file, (err) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+  })
+}
+
+module.exports ={
+  extractToJson
+}
