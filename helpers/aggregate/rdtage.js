1576fe6e1daec6107d1bd873b7aab119aabcb6ae
firmanjs <fimzhakim@gmail.com>
2020-12-14 16:02:21 +0700

    revision age grouping string to int

--- helpers/aggregate/rdtage.js
+++ helpers/aggregate/rdtage.js
@@ -1,30 +1,46 @@
-const { searching, rdtFilter } = require("./func/filter")
+const { searching, rdtFilter } = require('./func/filter')
+const { sumBetweenFunc } = require('./func')
+
+const converts = { $convert:{ input: '$age', to: 'double' } }
+
+const paramGroup = (group, value) => {
+  return {
+    $group: {
+      _id: group,
+      zero_ten: sumBetweenFunc({ $eq: ["$gender", value] }, converts, 0, 11),
+      ten_twenty: sumBetweenFunc({ $eq: ["$gender", value] }, converts, 11, 21),
+      twenty_thirty: sumBetweenFunc({ $eq: ["$gender", value] }, converts, 21, 31),
+      thirty_forty: sumBetweenFunc({ $eq: ["$gender", value] }, converts, 31, 41),
+      forty_fifty: sumBetweenFunc({ $eq: ["$gender", value] }, converts, 41, 51),
+      fifty_sixty: sumBetweenFunc({ $eq: ["$gender", value] }, converts, 51, 61),
+      sixty_seventy: sumBetweenFunc({ $eq: ["$gender", value] }, converts, 61, 71),
+      seventy_eighty: sumBetweenFunc({ $eq: ["$gender", value] }, converts, 71, 81),
+      eighty_ninety: sumBetweenFunc({ $eq: ["$gender", value] }, converts, 81, 91),
+      ninety_hundred: sumBetweenFunc({ $eq: ["$gender", value] }, converts, 91, 101),
+    }
+  }
+}
 
 const conditionAge = async (query, user) => {
   const search = await searching(query, user)
   const filter = rdtFilter(query)
-  const conditions = [{ $match: {
-    $and: [search, { ...filter }]
+  const conditions = [{
+    $match: {
+      $and: [search, { ...filter }]
     }
-  },{
-    $project: {
-      "range": {
-        $concat: [
-          { $cond: [{ $and: [{ $gt: ["$age", 0] }, { $lt: ["$age", 5] }] }, "under_five", ""] },
-          { $cond: [{ $and: [{ $gte: ["$age", 6] }, { $lt: ["$age", 20] }] }, "six_nine", ""] },
-          { $cond: [{ $and: [{ $gte: ["$age", 20] }, { $lt: ["$age", 30] }] }, "twenty_twenty_nine", ""] },
-          { $cond: [{ $and: [{ $gte: ["$age", 30] }, { $lt: ["$age", 40] }] }, "thirty_thirty_nine", ""] },
-          { $cond: [{ $and: [{ $gte: ["$age", 40] }, { $lt: ["$age", 50] }] }, "forty_forty_nine", ""] },
-          { $cond: [{ $and: [{ $gte: ["$age", 50] }, { $lt: ["$age", 60] }] }, "fifty_fifty_nine", ""] },
-          { $cond: [{ $and: [{ $gte: ["$age", 60] }, { $lt: ["$age", 70] }] }, "sixty_sixty_nine", ""] },
-          { $cond: [{ $and: [{ $gte: ["$age", 70] }, { $lt: ["$age", 80] }] }, "seventy_seventy_nine", ""] },
-          { $cond: [{ $gte: ["$age", 80] }, "grather_eighty", ""] }
-        ]
-      }
+  },
+  {
+    '$facet': {
+      'male': [paramGroup('male', 'L')],
+      'female': [paramGroup('female', 'P')]
     }
-  }, {
-    $group: { "_id": "$range", count: { $sum: 1 }}
-  }
+  },
+  {
+    '$project': {
+      'male': '$male',
+      'female': '$female'
+    }
+  },
   ]
   return conditions
 }
