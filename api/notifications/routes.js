2359195d177fca7d97faa72e342f01790216d97e
Khi Hady Sucahyo <khihady.ks@gmail.com>
2020-12-17 17:16:14 +0700

    added separete api notifications

--- api/notifications/routes.js
+++ api/notifications/routes.js
@@ -1,28 +1,23 @@
 module.exports = (server) => {
   const handlers = require('./handlers')(server)
 
+  const route = (method, path, callback)  => {
+    return  {
+      method: method,
+      path: path,
+      config: {
+        auth: 'jwt',
+        description: ` ${method} notifications`,
+        tags: ['notifications'],
+      },
+      handler: handlers[callback],
+    }
+  }
+
+
   return [
      // Get user notifications
-    {
-      method: 'GET',
-      path: '/notifications',
-      config: {
-        auth: 'jwt',
-        description: 'Get user notifications by user id',
-        tags: ['notifications'],
-      },
-      handler: handlers.getUserNotifications
-    },
-    // Get user notifications detail
-    {
-      method: 'PUT',
-      path: '/notifications/read',
-      config: {
-        auth: 'jwt',
-        description: 'mark as read notification',
-        tags: ['notifications'],
-      },
-      handler: handlers.markAsRead
-    },
+     route('GET', '/notifications', 'getUserNotifications'),
+     route('PUT', '/notifications/read', 'markAsRead'),
   ]
 }
