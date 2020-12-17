f4d70967568bbae9ab5866d7bb99343f58a9eae0
firmanjs <fimzhakim@gmail.com>
2020-12-17 10:14:21 +0700

    multiple update condtion history

--- services/cases_other.js
+++ services/cases_other.js
@@ -6,13 +6,17 @@ const { HISTORY_DEFAULT_SORT } = require('../helpers/constant')
 const conditional = async (result, payload, val) => {
   if (result.length > 0) {
     result.map(async res => {
+      const _id = res._id
       delete res._id;
       res.status = payload.status,
       res.stage = res.stage,
       res.final_result = payload.final_result,
       res.last_date_status_patient = payload.last_date_status_patient,
-      res.current_location_type = 'OTHERS'
-      await History.create(res)
+      res.current_location_type = res.current_location_type
+      const getData = await History.create(res)
+      await Case.updateOne({'_id': ObjectId(_id)}, {
+        $set: { 'last_history': getData._id }}
+      )
     })
   } else {
     const bodys = {
@@ -22,7 +26,10 @@ const conditional = async (result, payload, val) => {
       last_date_status_patient: payload.last_date_status_patient,
       current_location_type: 'OTHERS'
     }
-    await History.create(bodys)
+    const getData = await History.create(bodys)
+    await Case.updateOne({'_id': ObjectId(val)}, {
+      $set: { 'last_history': getData._id }}
+    )
   }
 }
 
@@ -38,6 +45,7 @@ const multipleUpdate = async (payload, user, callback) => {
 
     for (let val of maping) {
       const result = await History.find({ case: val })
+        .where('delete_status').ne('deleted')
         .sort(HISTORY_DEFAULT_SORT).limit(1).lean()
       await conditional(result, payload, val)
     }
