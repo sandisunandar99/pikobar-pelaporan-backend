1223a449c9a555dcc2c6491d5aa68795bf26e81c
firmanjs <fimzhakim@gmail.com>
2021-01-12 16:27:15 +0700

    change labeling pcr

--- helpers/aggregate/rdtaggregate.js
+++ helpers/aggregate/rdtaggregate.js
@@ -3,45 +3,36 @@ const { dateFilter } = require('../filter/date')
 const { byMonth, byMonthRdt, byMonthPcr } = require('./groupaggregate')
 const { MONTH } = require('../constant')
 
+const month = MONTH.EN
+let newArray = []
+let newArrayRdt = []
+let newArrayPcr = []
+
 const validationDataMonth = () => {
-  const month = MONTH.EN
-  let newArray = []
-  let newArrayRdt = []
-  let newArrayPcr = []
-
   for (let key in month) {
     let obj = {}
-      obj['_id'] = parseInt(key) + parseInt(1)
-      obj['name'] = month[key]
-      obj['rdt'] = 0
-      obj['pcr'] = 0
-      newArray.push(obj);
+    let objRdt = {}
+    let objPcr = {}
+    obj['_id'] = parseInt(key) + parseInt(1)
+    obj['name'] = month[key]
+    obj['rdt'] = 0
+    obj['pcr'] = 0
+    newArray.push(obj)
+    objRdt['_id'] = parseInt(key) + parseInt(1)
+    objRdt['name'] = month[key]
+    objRdt['reaktif'] = 0
+    objRdt['non_reaktif'] = 0
+    objRdt['inkonkuslif'] = 0
+    newArrayRdt.push(objRdt)
+    objPcr['_id'] = parseInt(key) + parseInt(1)
+    objPcr['name'] = month[key]
+    objPcr['positif'] = 0
+    objPcr['negatif'] = 0
+    objPcr['invalid'] = 0
+    newArrayPcr.push(objPcr)
   }
-
-  for (let key in month) {
-    let obj = {}
-      obj['_id'] = parseInt(key) + parseInt(1)
-      obj['name'] = month[key]
-      obj['reaktif'] = 0
-      obj['non_reaktif'] = 0
-      obj['inkonkuslif'] = 0
-      newArrayRdt.push(obj);
-  }
-
-  for (let key in month) {
-    let obj = {}
-      obj['_id'] = parseInt(key) + parseInt(1)
-      obj['name'] = month[key]
-      obj['positif'] = 0
-      obj['negatif'] = 0
-      obj['invalid'] = 0
-      newArrayPcr.push(obj);
-  }
-
   return {
-    'months' : newArray,
-    'monthrdt': newArrayRdt,
-    'monthpcr': newArrayPcr
+    'months' : newArray, 'monthrdt': newArrayRdt, 'monthpcr': newArrayPcr
   }
 }
 
