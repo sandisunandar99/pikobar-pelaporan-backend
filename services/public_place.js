61d7642fac3d8038c812e9e0063a22b1e80e7702
firmanjs <fimzhakim@gmail.com>
2021-01-20 11:27:28 +0700

    public place restore

--- services/public_place.js
+++ services/public_place.js
@@ -4,19 +4,19 @@ const { findGlobal, deleteGlobal } = require('../helpers/global/crud')
 
 const createPublicPlace = async (payload, id_case, callback) => {
   try {
-    const addToSet = {
-      "visited_public_place": {
-        "public_place_category": payload.public_place_category,
-        "public_place_name": payload.public_place_name,
-        "public_place_address": payload.public_place_address,
-        "public_place_date_visited": payload.public_place_date_visited,
-        "public_place_duration_visited": payload.public_place_duration_visited
-      }
-    }
     const inserted = await PublicPlace.updateOne(
       { "_id": ObjectId(id_case) },
       { $set: { 'has_visited_public_place': true },
-      $addToSet: addToSet }, { new: true })
+        $addToSet: {
+          "visited_public_place": {
+            "public_place_category": payload.public_place_category,
+            "public_place_name": payload.public_place_name,
+            "public_place_address": payload.public_place_address,
+            "public_place_date_visited": payload.public_place_date_visited,
+            "public_place_duration_visited": payload.public_place_duration_visited
+          }
+        }
+      }, { new: true })
     callback(null, inserted)
   } catch (error) {
     callback(error, null)
@@ -36,17 +36,16 @@ const listPublicPlace = async (id_case, callback) => {
 
 const updatePublicPlace = async (id_public_place, payload, callback) => {
   try {
-    const set = {
+    const updated = await PublicPlace.updateOne({
+      "visited_public_place._id": ObjectId(id_public_place)
+    },
+    { "$set": {
       "visited_public_place.$.public_place_category": payload.public_place_category,
       "visited_public_place.$.public_place_name": payload.public_place_name,
       "visited_public_place.$.public_place_address": payload.public_place_address,
       "visited_public_place.$.public_place_date_visited": payload.public_place_date_visited,
       "visited_public_place.$.public_place_duration_visited": payload.public_place_duration_visited
-    }
-    const updated = await PublicPlace.updateOne({
-      "visited_public_place._id": ObjectId(id_public_place)
-    },
-    { "$set": set }, { new : true })
+    }}, { new : true })
     callback(null, updated)
   } catch (error) {
     callback(error, null)
