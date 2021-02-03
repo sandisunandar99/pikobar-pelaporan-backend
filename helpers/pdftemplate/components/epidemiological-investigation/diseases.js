788e4b2d00cffb65bb9ede4e38bef0ecd0b08347
firmanjs <fimzhakim@gmail.com>
2021-02-03 08:41:33 +0700

    refactoring diseases helper PE

--- helpers/pdftemplate/components/epidemiological-investigation/diseases.js
+++ helpers/pdftemplate/components/epidemiological-investigation/diseases.js
@@ -1,61 +1,45 @@
 
-const moment = require('moment')
+const tdBody = (textHeader, textBorder, diseases) => {
+  return [
+    { text: `${textHeader}`, border: [] },
+    { text: `: [${isTrue(textHeader.toLowerCase(), diseases)}] Ya   [${isFalse(textHeader.toLowerCase(), diseases)}] Tdk`, border: [] },
+    { text: `${textBorder}`, border: [] },
+    { text: `: [${isTrue(textBorder.toLowerCase(), diseases)}] Ya   [${isFalse(textBorder.toLowerCase(), diseases)}] Tdk`, border: [] },
+  ]
+}
+
+const isTrue = (value, diseases) => {
+  return diseases.includes(value) ? '√' : '  '
+}
+
+const isFalse = (value, diseases) => {
+  return !diseases.includes(value) ? '√' : '  '
+}
+
 const render = (data) => {
-  
   const diseases = data.last_history.diseases.map(s => s.toLowerCase())
-
-  const isTrue = (value) => {
-    return diseases.includes(value) ? '√' : '  '
-  }
-
-  const isFalse = (value) => {
-    return !diseases.includes(value) ? '√' : '  '
-  }
-
   return [
     {
       colSpan: 4,
       table: {
         widths: [112, 135, 110, 125],
         body: [
-          [
-            { text: `Hamil`, border: [] },
-            { text: `: [${isTrue('hamil')}] Ya   [${isFalse('hamil')}] Tdk`, border: [] },
-            { text: `Gangguan imunologi`, border: [] },
-            { text: `: [${isTrue('gangguan imunologi')}] Ya   [${isFalse('gangguan imunologi')}] Tdk`, border: [] },
-          ],
-          [
-            { text: `Diabetes`, border: [] },
-            { text: `: [${isTrue('diabetes')}] Ya   [${isFalse('diabetes')}] Tdk`, border: [] },
-            { text: `Gagal ginjal kronis`, border: [] },
-            { text: `: [${isTrue('gagal ginjal kronis')}] Ya   [${isFalse('gagal ginjal kronis')}] Tdk`, border: [] },
-          ],
-          [
-            { text: `Penyakit jantung`, border: [] },
-            { text: `: [${isTrue('penyakit jantung')}] Ya   [${isFalse('penyakit jantung')}] Tdk`, border: [] },
-            { text: `Gagal hati kronis`, border: [] },
-            { text: `: [${isTrue('gagal hati kronis')}] Ya   [${isFalse('gagal hati kronis')}] Tdk`, border: [] },
-          ],
-          [
-            { text: `Hipertensi`, border: [] },
-            { text: `: [${isTrue('hipertensi')}] Ya   [${isFalse('hipertensi')}] Tdk`, border: [] },
-            { text: `PPOK`, border: [] },
-            { text: `: [${isTrue('ppok')}] Ya   [${isFalse('ppok')}] Tdk`, border: [] },
-          ],
+          tdBody('Hamil', 'Gangguan imunologi', diseases),
+          tdBody('Diabetes', 'Gagal ginjal kronis', diseases),
+          tdBody('Penyakit jantung', 'Gagal hati kronis', diseases),
+          tdBody('Hipertensi', 'PPOK', diseases),
           [
             { text: `Keganasan`, border: [] },
-            { text: `: [${isTrue('keganasan')}] Ya   [${isFalse('keganasan')}] Tdk`, border: [] },
+            { text: `: [${isTrue('keganasan', diseases)}] Ya   [${isFalse('keganasan', diseases)}] Tdk`, border: [] },
             { text: `Lainnya (sebutkan)`, border: [] },
             { text: `: ${data.last_history.diseases_other || '-'}`, border: [] },
           ],
         ],
       },
       layout: {
-        paddingLeft: (i, node) => 0,
-        paddingTop: (i, node) => -0.2,
+        paddingLeft: (i, node) => 0, paddingTop: (i, node) => -0.2,
         paddingBottom: (i, node) => 0
       }
-      // 
     },{},{},{}
   ]
 }
