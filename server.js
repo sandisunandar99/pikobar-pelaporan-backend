43263591fa1179e4f553ef4b86407d45215abafe
sandi <sandisunandar99@gmail.com>
2021-02-16 14:42:21 +0700

    re define pubsub

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const Glue = require('glue');
 const manifest = require('./config/manifest');
 const config = require('./config/config')
 const Sentry = require("@sentry/node")
-const schedule = require('node-schedule')
 
 if (!process.env.PRODUCTION) {
   manifest.registrations.push({
@@ -13,53 +12,6 @@ if (!process.env.PRODUCTION) {
     }
   });
 }
-
-schedule.scheduleJob("*/1 * * * *", function() {
-  console.log('This runs every 1 minutes')
-
-  const { PubSub } = require('@google-cloud/pubsub');
-  require('dotenv').config()
-
-  const credentials = {
-    client_email: process.env.CLIENT_EMAIL,
-    private_key: process.env.PRIVATE_KEY,
-  }
-
-  const projectId = process.env.PROJECT_ID
-  const subscriptionName = process.env.SUBSCRIPTION_NAME
-  let timeout = 60
-
-  timeout = Number(timeout);
-
-  const pubSubClient = new PubSub({ projectId, credentials });
-
-  function listenForMessages() {
-    const subscription = pubSubClient.subscription(subscriptionName);
-
-    let messageCount = 0;
-    const messageHandler = (message) => {
-      console.log(`Received message ${message.id}:`);
-      console.log(`\tData: ${message.data}`);
-      console.log(`\tAttributes: ${message.attributes}`);
-      messageCount += 1;
-
-      // "Ack" (acknowledge receipt of) the message
-      message.ack();
-    };
-
-    // Listen for new messages until timeout is hit
-    subscription.on('message', messageHandler);
-
-    setTimeout(() => {
-      subscription.removeListener('message', messageHandler);
-      console.log(`${messageCount} message(s) received.`);
-    }, timeout * 1000);
-  }
-
-  listenForMessages();
-  // [END pubsub_subscriber_async_pull]
-  // [END pubsub_quickstart_subscriber]
-});
 
 Glue.compose(manifest, { relativeTo: __dirname }, (err, server) => {
   if (err) {
