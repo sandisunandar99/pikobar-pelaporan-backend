2ffb705c0c8919a544edbdcd171170c327020fde
sandi <sandisunandar99@gmail.com>
2021-02-16 14:42:53 +0700

    get pubsub realtime per 1 minutes

---
+++ integration/index.js
@@ -0,0 +1,46 @@
+const schedule = require('node-schedule')
+
+const register = (server, options, next) => {
+  schedule.scheduleJob("*/1 * * * *", function() {
+    console.log('This runs every 1 minutes')
+    const { PubSub } = require('@google-cloud/pubsub');
+    const pubsubClient = new PubSub()
+    const subscriptionName = process.env.SUBSCRIPTION_NAME
+    let timeout = 60
+    let msgCount = 0
+
+    try {
+      const subscriber = pubsubClient.subscription(subscriptionName)
+      const msgHandler = message => {
+        console.log(`Received message ${message.id}:`);
+        console.log(`\tData: ${message.data}`);
+        console.log(`\tAttributes: ${message.attributes}`);
+        msgCount += 1;
+
+        /**
+         * TODO: create function service for get data from pub sub
+         * code create here
+         */
+
+        message.ack();
+      }
+
+      subscriber.on('message', msgHandler)
+
+      setTimeout(() => {
+            subscriber.removeListener('message', msgHandler);
+            console.log(`${msgCount} message(s) received.`);
+        }, timeout * 1000);
+
+    } catch (error) {
+      console.log(`ERROR PUBSUB: ${error}`);
+    }
+
+  });
+}
+
+register.attributes = {
+  pkg: require('./package.json')
+}
+
+module.exports = register
