23979acb746dd88794e953b39579ca16eb7dcb9c
firmanjs <fimzhakim@gmail.com>
2021-02-19 09:48:44 +0700

    fix cognitive complex

--- utils/sort.js
+++ utils/sort.js
@@ -2,8 +2,9 @@ const sortCondition = (query) => {
   let sort = { updatedAt: -1 };
   if (query.sort && query.sort.split) {
     let splits = query.sort.split(':')
-    sort.last_date_status_patient = splits[1] === 'desc' ? -1 : 1
-    sort[splits[0]] = splits[1] === 'desc' ? -1 : 1
+    const sorted = splits[1] === 'desc' ? -1 : 1
+    sort.last_date_status_patient = sorted
+    sort[splits[0]] = sorted
   }
 
   return sort
