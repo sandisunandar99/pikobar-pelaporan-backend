c7b3f9092ac0100fce27e49d23dfba078a663374
firmanjs <fimzhakim@gmail.com>
2021-02-22 14:21:15 +0700

    resolve conflict

--- utils/index.js
+++ utils/index.js
@@ -2,10 +2,6 @@ module.exports = {
   ...require('./sql'),
   ...require('./column'),
   ...require('./select'),
-<<<<<<< HEAD
   ...require('./lookup'),
-=======
-  ...require('./lookup')
->>>>>>> 60715df001c61b2b52b248bd7198dcad98e38a3e
   ...require('./sort')
 }
