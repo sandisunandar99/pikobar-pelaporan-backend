966babc8e1ad0340256b7c425dd84ef69a7032b4
firmanjs <fimzhakim@gmail.com>
2021-02-22 14:19:51 +0700

    cognitive complex

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,7 @@
 module.exports = {
   ...require('./sql'),
   ...require('./column'),
-<<<<<<< HEAD
   ...require('./select'),
-  ...require('./lookup')
-=======
+  ...require('./lookup'),
   ...require('./sort')
->>>>>>> dd3ca884382b9034a8db9abfdd119afa854107da
 }
