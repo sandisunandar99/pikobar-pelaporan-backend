e7e1900471005d0cabedbb8fd33071359d1bb255
sandi <sandisunandar99@gmail.com>
2021-02-25 10:11:51 +0700

    TODO: logging simpan nik, nama, notelp

--- services/integration.js
+++ services/integration.js
@@ -3,6 +3,8 @@ const {findUserCases, transformDataPayload} = require('../helpers/integration')
 const createInfoClinics = async (payload) => {
   const data = JSON.parse(payload)
   try {
+    //TODO: membuat logging unutk menyimpan data jika nik/ no telp tidak di temukan
+    // di laporan kasus
     //check data by nik or phone number
     const patient = await findUserCases(data)
     // transform payload with last data case patient
