8f0dd192c14aa1a57f4309e96e86f156fb8a478b
sandi <sandisunandar99@gmail.com>
2021-03-01 14:48:43 +0700

    update models

--- models/LogSelfReport.js
+++ models/LogSelfReport.js
@@ -7,15 +7,4 @@ const LogSelfReport = new mongoose.Schema({
    phone_number: String
 })
 
-
-LogSelfReport.methods.toJSONFor = function () {
-    return {
-      user_id: this.User_id,
-      name: this.Name,
-      nik: this.Nik,
-      phone_number: this.Phone_number
-    }
-}
-
-
 module.exports = mongoose.model('LogSelfReport', LogSelfReport)
