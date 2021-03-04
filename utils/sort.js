605a337c1497f3732dcf7af24580c640c32cb57e
firmanjs <fimzhakim@gmail.com>
2021-03-04 10:23:16 +0700

    change let to const

--- utils/sort.js
+++ utils/sort.js
@@ -1,7 +1,7 @@
 const sortCondition = (query) => {
-  let sort = { updatedAt: -1 };
+  const sort = { updatedAt: -1 };
   if (query.sort && query.sort.split) {
-    let splits = query.sort.split(':')
+    const splits = query.sort.split(':')
     const sorted = splits[1] === 'desc' ? -1 : 1
     sort.last_date_status_patient = sorted
     sort[splits[0]] = sorted
