7e21b71682cacf4cea53ec0f934ddf1e73420c0f
firmanjs <fimzhakim@gmail.com>
2021-03-06 21:17:36 +0700

    initial redis for chaching

---
+++ config/redis.js
@@ -0,0 +1,13 @@
+const redis = require('redis')
+const clientConfig = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
+})
+
+clientConfig.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+module.exports = {
+  clientConfig
+}
