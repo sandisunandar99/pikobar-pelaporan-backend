f633c462e4c3e520bae7557449f8443b452b4b38
firmanjs <fimzhakim@gmail.com>
2021-03-10 14:21:05 +0700

    add logging

--- helpers/redis/index.js
+++ helpers/redis/index.js
@@ -7,9 +7,11 @@ const setCacheRedis = (key, data, expireTime, callback) => {
       if(result){
         const resultJSON = JSON.parse(result)
         callback(null, resultJSON)
+        console.info('redis source')
       }else{
         clientConfig.setex(key, expireTime, JSON.stringify(data)) // set redis key
         callback(null, data)
+        console.info('api source')
       }
     })
   } catch (error) {
