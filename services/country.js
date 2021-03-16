43da5f451777b941b3a6208e2c25f1caa274caae
firmanjs <fimzhakim@gmail.com>
2021-03-16 12:46:07 +0700

    change get data

--- services/country.js
+++ services/country.js
@@ -1,12 +1,22 @@
-const { setCacheRedis } = require('../helpers/redis')
+const { clientConfig } = require('../config/redis')
 
 const getCountryList = (callback) => {
   const fs = require("fs");
   const path = require("path");
   try {
-    const obj = JSON.parse(fs.readFileSync(path.join("helpers", "listcountry.json"), "utf8"));
     const expireTime = 1440 * 60 * 1000 // 24 hours expire
-    setCacheRedis('country', obj, expireTime, callback)
+    clientConfig.get('country', (err, result) => {
+      if(result){
+        const resultJSON = JSON.parse(result)
+        callback(null, resultJSON)
+        console.info('redis source')
+      }else{
+        const obj = JSON.parse(fs.readFileSync(path.join("helpers", "listcountry.json"), "utf8"));
+        clientConfig.setex(key, expireTime, JSON.stringify(obj)) // set redis key
+        callback(null, obj)
+        console.info('api source')
+      }
+    })
   } catch (error) {
     callback(error, null)
   }
