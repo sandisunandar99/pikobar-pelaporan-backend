539d09f539d5d879e06d3c9c8915e03c378e7261
firmanjs <fimzhakim@gmail.com>
2021-03-22 17:38:47 +0700

    allowed 25 lines

--- helpers/job/index.js
+++ helpers/job/index.js
@@ -1,5 +1,4 @@
 const Queue = require('bee-queue')
-const fs = require('fs')
 const { sendEmailWithAttachment } = require('../email')
 const { updateLogJob } = require('./log')
 const options = {
@@ -8,6 +7,16 @@ const options = {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
   },
+}
+
+const emailOptions = (resultJob) => {
+  const fs = require('fs')
+  return [{
+    filename: resultJob.filename,
+    content: fs.createReadStream(resultJob.path),
+    path: resultJob.path,
+    contentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+  }]
 }
 
 const createJobQueue = async (nameQueue, query, user, method, message, time) => {
@@ -23,18 +32,10 @@ const createJobQueue = async (nameQueue, query, user, method, message, time) => 
         const resultJob = await method(query, user, job.id)
         console.log(`🧾 Success : Waiting for sending email`)
 
-        // notify job and attempt to send the mail
-        const options = [{
-          filename: resultJob.filename,
-          content: fs.createReadStream(resultJob.path),
-          path: resultJob.path,
-          contentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        }]
-
         await updateLogJob(job.id, { job_progress: 85 }) // notify job progress and save
         done()
         clearInterval(timer)
-        sendEmailWithAttachment(message, options, query.email, resultJob.path, job.id)
+        sendEmailWithAttachment(message, emailOptions(resultJob), query.email, resultJob.path, job.id)
       }, time * 60 * 1000)
     })
   } catch (error) {
@@ -44,7 +45,6 @@ const createJobQueue = async (nameQueue, query, user, method, message, time) => 
     }
     await updateLogJob(job.id, param) // save job error
   }
-
 }
 
 module.exports = {
