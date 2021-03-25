52cecf704f4aa3435270029a42ceb8f52823fa5d
sandi <sandisunandar99@gmail.com>
2021-03-25 15:08:25 +0700

    initiap pubsub

--- integration/index.js
+++ integration/index.js
@@ -1,19 +1,18 @@
 const schedule = require('node-schedule')
 const { PubSub } = require('@google-cloud/pubsub')
 const { pubsub } = require('../config/config')
-const credential = pubsub.credentials
-const projectId = process.env.GCP_PROJ_ID
-const subscriptionName = process.env.SUBSCRIPTION_NAME
+const laporMandiriSub = process.env.SUBSCRIPTION_NAME
+const labkesPelaporanSub = process.env.SUBSCRIPTION_NAME2
 const pubsubClient = new PubSub(pubsub)
 const timeout = 60
 let msgCount = 0
 
-const register = (server, options, next) => {
-  schedule.scheduleJob("*/1 * * * *", function() {
-    console.log('Worker Google Pub/Sub runs every 1 minutes')
+const register = async(server, options, next) => {
+  const LaporMandiri = await schedule.scheduleJob("*/1 * * * *", function() {
+    console.log('Worker Pikobar Lapor Mandiri runs every 1 minutes')
 
     try {
-      const subscriber = pubsubClient.subscription(subscriptionName)
+      const subscriber = pubsubClient.subscription(laporMandiriSub)
       const msgHandler = async (message) => {
           msgCount += 1;
           try {
@@ -35,6 +34,34 @@ const register = (server, options, next) => {
       console.log(`ERROR PUBSUB: ${error}`);
     }
   });
+
+  const LabkesPelaporan = await schedule.scheduleJob("*/1 * * * *", function() {
+    console.log('Worker Labkes Pelaporan runs every 1 minutes')
+
+    try {
+      const subscriber = pubsubClient.subscription(labkesPelaporanSub)
+      const msgHandler = async (message) => {
+          msgCount += 1;
+          try {
+            const data = Buffer.from(message.data, 'base64').toString()
+            console.log(data);
+
+            message.ack();
+          } catch (error) {console.log(error)}
+      }
+
+      subscriber.on('message', msgHandler)
+      setTimeout(() => {
+            subscriber.removeListener('message', msgHandler);
+            console.log(`${msgCount} message(s) received.`);
+      }, timeout * 1000);
+
+    } catch (error) {
+      console.log(`ERROR PUBSUB: ${error}`);
+    }
+  });
+
+  Promise.all([LaporMandiri, LabkesPelaporan])
 
   return next()
 }
