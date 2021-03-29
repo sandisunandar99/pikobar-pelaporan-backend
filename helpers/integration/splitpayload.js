0fe5be6720d3b77e98adb65055a18acfd92e5ad8
sandi <sandisunandar99@gmail.com>
2021-03-29 11:00:55 +0700

    split

--- helpers/integration/splitpayload.js
+++ helpers/integration/splitpayload.js
@@ -106,7 +106,6 @@ const payloadLabkes = (payloadLabkes) => {
   }
   return Obj
 }
-
 const payloadLabkes2 = (payloadLabkes) => {
   const Obj = {
     current_location_type: "RUMAH",
@@ -125,89 +124,8 @@ const payloadLabkes2 = (payloadLabkes) => {
   return Obj
 }
 
-
-
-const splitCasePayload = (payloadLabkes) => {
+const splitCasePayload1 = (payloadLabkes) => {
   const Obj = {
-    id_case_national: "",
-    id_case_related: "",
-    name_case_related: "",
-    is_west_java: true,
-    is_nik_exists: true,
-    is_phone_number_exists: true,
-    address_province_code: "32",
-    address_province_name: "Jawa Barat",
-    nationality_name: "",
-    occupation: "Belum Bekerja",
-    office_address: "",
-    status: "CLOSECONTACT",
-    stage: "",
-    final_result: "5",
-    report_source: "",
-    there_are_symptoms: false,
-    diagnosis: [],
-    diagnosis_other: "",
-    diseases: [],
-    diseases_other: "",
-    first_symptom_date: "",
-    history_tracing: [],
-    is_went_abroad: false,
-    visited_country: "",
-    return_date: "",
-    is_went_other_city: false,
-    visited_city: "",
-    is_contact_with_positive: false,
-    history_notes: "",
-    other_notes: "",
-    interviewers_name: "",
-    interviewers_phone_number: "",
-    interview_date: "",
-    name_parents: "",
-    note_nik: "",
-    note_phone_number: "",
-    diagnosis_ards: 2,
-    diagnosis_covid: 2,
-    diagnosis_pneumonia: 2,
-    other_diagnosis: "",
-    serum_check: false,
-    sputum_check: false,
-    swab_check: false,
-    physical_check: "",
-    pysichal_activity: "",
-    smoking: 2,
-    consume_alcohol: 2,
-    income: "",
-    travel: "",
-    visited: "",
-    start_travel: "",
-    end_travel: "",
-    close_contact: 2,
-    close_contact_confirm: 2,
-    close_contact_animal_market: 2,
-    close_contact_public_place: 2,
-    close_contact_medical_facility: 2,
-    close_contact_heavy_ispa_group: false,
-    close_contact_health_worker: false,
-    apd_use: [],
-    name_close_contact: "",
-    id_close_contact: "",
-    name_close_contact_confirm: "",
-    id_close_contact_confirm: "",
-    close_contact_patient: [],
-    inspection_support: [],
-    close_contacted_before_sick_14_days: false,
-    travelling_history_before_sick_14_days: false,
-    travelling_history: [],
-    visited_local_area_before_sick_14_days: false,
-    visited_local_area: [],
-    has_visited_public_place: false,
-    visited_public_place: [],
-    area_transmision: [],
-    close_contact_premier: [],
-    is_other_diagnosisr_respiratory_disease: false,
-    close_contact_have_pets: false,
-    close_contact_performing_aerosol_procedures: false,
-    other_diagnosisr_respiratory_disease: "",
     physical_check_temperature: "",
     physical_check_blood_pressure: "",
     physical_check_pulse: "",
@@ -231,7 +149,108 @@ const splitCasePayload = (payloadLabkes) => {
   return Obj
 }
 
+const splitCasePayload2 = (payloadLabkes) => {
+  const Obj = {
+    id_case_national: "",
+    id_case_related: "",
+    name_case_related: "",
+    is_west_java: true,
+    is_nik_exists: true,
+    is_phone_number_exists: true,
+    address_province_code: "32",
+    address_province_name: "Jawa Barat",
+    nationality_name: "",
+    occupation: "Belum Bekerja",
+    office_address: "",
+    status: "CLOSECONTACT",
+    stage: "",
+    final_result: "5",
+    report_source: "",
+    there_are_symptoms: false,
+    diagnosis: [],
+    diagnosis_other: "",
+    diseases: [],
+    diseases_other: "",
+  }
+  return Obj
+}
 
+const splitCasePayload3 = (payloadLabkes) => {
+  const Obj = {
+    first_symptom_date: "",
+    history_tracing: [],
+    is_went_abroad: false,
+    visited_country: "",
+    return_date: "",
+    is_went_other_city: false,
+    visited_city: "",
+    is_contact_with_positive: false,
+    history_notes: "",
+    other_notes: "",
+    interviewers_name: "",
+    interviewers_phone_number: "",
+    interview_date: "",
+    name_parents: "",
+    note_nik: "",
+    note_phone_number: "",
+    diagnosis_ards: 2,
+    diagnosis_covid: 2,
+    diagnosis_pneumonia: 2,
+    other_diagnosis: "",
+  }
+  return Obj
+}
+
+const splitCasePayload4 = (payloadLabkes) => {
+  const Obj = {
+    serum_check: false,
+    sputum_check: false,
+    swab_check: false,
+    physical_check: "",
+    pysichal_activity: "",
+    smoking: 2,
+    consume_alcohol: 2,
+    income: "",
+    travel: "",
+    visited: "",
+    start_travel: "",
+    end_travel: "",
+    close_contact: 2,
+    close_contact_confirm: 2,
+    close_contact_animal_market: 2,
+    close_contact_public_place: 2,
+    close_contact_medical_facility: 2,
+    close_contact_heavy_ispa_group: false,
+    close_contact_health_worker: false,
+    apd_use: [],
+  }
+  return Obj
+}
+
+const splitCasePayload5 = (payloadLabkes) => {
+  const Obj = {
+    name_close_contact: "",
+    id_close_contact: "",
+    name_close_contact_confirm: "",
+    id_close_contact_confirm: "",
+    close_contact_patient: [],
+    inspection_support: [],
+    close_contacted_before_sick_14_days: false,
+    travelling_history_before_sick_14_days: false,
+    travelling_history: [],
+    visited_local_area_before_sick_14_days: false,
+    visited_local_area: [],
+    has_visited_public_place: false,
+    visited_public_place: [],
+    area_transmision: [],
+    close_contact_premier: [],
+    is_other_diagnosisr_respiratory_disease: false,
+    close_contact_have_pets: false,
+    close_contact_performing_aerosol_procedures: false,
+    other_diagnosisr_respiratory_disease: "",
+  }
+  return Obj
+}
 
 module.exports = {
   PayloadLaporMandri,
@@ -240,5 +259,9 @@ module.exports = {
   splitPayload3,
   payloadLabkes,
   payloadLabkes2,
-  splitCasePayload
+  splitCasePayload1,
+  splitCasePayload2,
+  splitCasePayload3,
+  splitCasePayload4,
+  splitCasePayload5,
 }
