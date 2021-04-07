8921da4bfb35390d978f8a63ce8eda30098190ec
sandi <sandisunandar99@gmail.com>
2021-04-07 20:24:35 +0700

    move scheduller proces

--- integration/index.js
+++ integration/index.js
@@ -1,6 +1,9 @@
-const register = async(server, options, next) => {
+const schedule = require('node-schedule')
+const register = (server, options, next) => {
+  schedule.scheduleJob("*/1 * * * *", async function() {
     await require('./lapor_mandiri')(server)
     await require('./labkes_pelaporan')(server)
+  })
   return next()
 }
 
--- integration/lapor_mandiri.js
+++ integration/lapor_mandiri.js
@@ -1,4 +1,3 @@
-const schedule = require('node-schedule')
 const {PubSub} = require('@google-cloud/pubsub')
 const {pubsub} = require('../config/config')
 const laporMandiriSub = process.env.SUBSCRIPTION_NAME_LAPORMANDIRI
@@ -6,29 +5,26 @@ const pubsubClient = new PubSub(pubsub)
 const {setTimeOut} = require('../helpers/integration/timeout')
 
 module.exports = (server) => {
-  schedule.scheduleJob("*/1 * * * *", function () {
-    console.log('Worker Pikobar Lapor Mandiri runs every 1 minutes')
-
-    try {
-      const subscriber = pubsubClient.subscription(laporMandiriSub)
-      const msgHandler = async (message) => {
-        const data = Buffer.from(message.data, 'base64').toString()
-        const services = server.methods.services
-        await server.methods.services.integration.createInfoClinics(data, services, (err, res)=>{console.log(`Data Pikobar Reveived.. ID : ${message.id} ---- ERR: ${err}`)})
-        message.ack();
-      }
-
-      const errorHandler = (error) => {
-        console.error(`ERROR: ${error}`);
-        throw error;
-      }
-
-      subscriber.on('error', errorHandler)
-      subscriber.on('message', msgHandler)
-      setTimeOut(laporMandiriSub, msgHandler, errorHandler)
-
-    } catch (error) {
-      console.log(`ERROR PUBSUB PIKOBAR: ${error}`);
+  console.log('Worker Pikobar Lapor Mandiri runs every 1 minutes')
+  try {
+    const subscriber = pubsubClient.subscription(laporMandiriSub)
+    const msgHandler = async (message) => {
+      const data = Buffer.from(message.data, 'base64').toString()
+      const services = server.methods.services
+      await server.methods.services.integration.createInfoClinics(data, services, (err, res)=>{console.log(`Data Pikobar Reveived.. ID : ${message.id} ---- ERR: ${err}`)})
+      message.ack();
     }
-  });
+
+    const errorHandler = (error) => {
+      console.error(`ERROR: ${error}`);
+      throw error;
+    }
+
+    subscriber.on('error', errorHandler)
+    subscriber.on('message', msgHandler)
+    setTimeOut(laporMandiriSub, msgHandler, errorHandler)
+
+  } catch (error) {
+    console.log(`ERROR PUBSUB PIKOBAR: ${error}`);
+  }
 }
