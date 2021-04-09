671eb97928c8a508f29d2540cd84ceb0d7f9aae6
firmanjs <fimzhakim@gmail.com>
2021-04-09 14:31:30 +0700

    update set key map

--- services/map.js
+++ services/map.js
@@ -10,9 +10,9 @@ const listMap = async (query, user, callback) => {
   if([ROLE.ADMIN, ROLE.PROVINCE].includes(user.role)){
     key = `list-map-${user.username}`
   }else if([ROLE.KOTAKAB].includes(user.role)){
-    key = `list-map-${user.code_district_city}`
+    key = `list-map-${user.username}-${user.code_district_city}`
   }else{
-    key = `list-map-${user.username}-${user.id}`
+    key = `list-map-${user.id}-${user.code_district_city}`
   }
   try {
     clientConfig.get(key, async (err, result) => {
