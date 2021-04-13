5272bd62ec773d2c88c7a9178c5ba03e40294fc6
sandi <sandisunandar99@gmail.com>
2021-04-13 14:02:09 +0700

    test of scheduller

--- integration/index.js
+++ integration/index.js
@@ -1,9 +1,9 @@
 const schedule = require('node-schedule')
 const register = (server, options, next) => {
-  schedule.scheduleJob("*/1 * * * *", async function() {
+  // schedule.scheduleJob("*/1 * * * *", async function() {
     await require('./lapor_mandiri')(server)
     await require('./labkes_pelaporan')(server)
-  })
+  // })
   return next()
 }
 
