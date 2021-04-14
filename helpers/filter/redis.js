a0cce83c12031758785b2b963fb8448ab51528aa
firmanjs <fimzhakim@gmail.com>
2021-04-14 13:11:31 +0700

    add documentation set key dashboard

--- helpers/filter/redis.js
+++ helpers/filter/redis.js
@@ -1,13 +1,22 @@
+/**
+ *
+ *
+ * @param {*} user
+ * @param {*} query
+ * @param {*} unique
+ * @return {string}
+ */
 const validateQueryDashboard = (user, query, unique) => {
-  const parseQuery = JSON.stringify(query)
-  const toJson = JSON.parse(parseQuery)
-  let str = []
+  const parseQuery = JSON.stringify(query) // parse query string nodejs to string
+  const toJson = JSON.parse(parseQuery) // parse string to object
+  let str = [] // define array
 
+  // loop for set to new arrray
   for (let prop in toJson) {
     str.push(toJson[prop])
   }
 
-  const joinStr = str.join('-')
+  const joinStr = str.join('-') // convert array to string with '-'
 
   if(query) {
     key = `${unique}-${user.username}-${joinStr}`
@@ -18,8 +27,17 @@ const validateQueryDashboard = (user, query, unique) => {
   return key
 }
 
+/**
+ *
+ *
+ * @param {*} query
+ * @param {*} user
+ * @param {*} time
+ * @param {*} keys
+ * @return {}
+ */
 const keyDashboard = (query, user, time, keys) => {
-  const expireTime = time * 60 // rules 1 minute = 60 seconds
+  /** @type {*} */
   const key = validateQueryDashboard(user, query, keys)
 
   return { key, expireTime }
