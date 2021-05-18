22a26c06f613d107e91846569289c2af668c788c
firmanjs <fimzhakim@gmail.com>
2021-05-18 09:14:54 +0700

    updated refactor code smells

--- helpers/filter/userfilter.js
+++ helpers/filter/userfilter.js
@@ -1,41 +1,36 @@
-'use strict';
-const check = require('../rolecheck');
+'use strict'
+const check = require('../rolecheck')
+const { ROLE } = require('../constant')
 const filterUser = (query, user) => {
-    let params = check.userByRole({}, user);
-    if (query.role) {
-        params.role = query.role;
-    }
-    if (user.role == "dinkesprov" || user.role == "superadmin") {
-        if (query.code_district_city) {
-            params.code_district_city = query.code_district_city;
-        }
-    }
-    if (query.address_village_code) {
-        params.address_village_code = query.address_village_code;
-    }
-    if (query.address_subdistrict_code) {
-        params.address_subdistrict_code = query.address_subdistrict_code;
-    }
-    return params;
+  let params = check.userByRole({}, user)
+  if (query.role) params.role = query.role
+  if (user.role == ROLE.PROVINCE || user.role == ROLE.ADMIN) {
+    if (query.code_district_city) params.code_district_city = query.code_district_city
+  }
+  if (query.address_village_code) params.address_village_code = query.address_village_code
+  if (query.address_subdistrict_code) {
+    params.address_subdistrict_code = query.address_subdistrict_code
+  }
+  return params
 }
 
 const searchUser = (query) => {
-    let search_params;
-    if (query.search) {
-        search_params = [
-            { username: new RegExp(query.search, "i") },
-            { fullname: new RegExp(query.search, "i") },
-            { email: new RegExp(query.search, "i") },
-            { phone_number: new RegExp(query.search, "i"), },
-            { address_street: new RegExp(query.search, "i"), },
-            { address_village_name: new RegExp(query.search, "i"), },
-        ];
-    } else {
-        search_params = {};
-    }
-    return search_params;
-};
+  let search_params
+  if (query.search) {
+    search_params = [
+      { username: new RegExp(query.search, "i") },
+      { fullname: new RegExp(query.search, "i") },
+      { email: new RegExp(query.search, "i") },
+      { phone_number: new RegExp(query.search, "i"), },
+      { address_street: new RegExp(query.search, "i"), },
+      { address_village_name: new RegExp(query.search, "i"), },
+    ]
+  } else {
+    search_params = {}
+  }
+  return search_params
+}
 
 module.exports = {
-    filterUser, searchUser
+  filterUser, searchUser
 }
