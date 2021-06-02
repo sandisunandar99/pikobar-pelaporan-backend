5afa8bdc78060e1c310144e737a44bb88065f9a5
sandi <sandisunandar99@gmail.com>
2021-06-02 11:48:24 +0700

    remove duplicate initial service

--- services/index.js
+++ services/index.js
@@ -31,8 +31,7 @@ const register = (server, options, next) => {
     require('./cases_other'),
     require('./rdt_others'),
     require('./integration'),
-    require('./queue'),
-    require('./rdt_others')
+    require('./queue')
   );
   server.method(services)
   return next()
