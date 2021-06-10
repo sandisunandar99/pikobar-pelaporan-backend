958277503b0349306ca405ce1b3b503848e8e307
firmanjs <fimzhakim@gmail.com>
2021-06-10 19:23:11 +0700

    inject rdt is not use

--- services/inject.js
+++ services/inject.js
@@ -55,71 +55,71 @@ const getCountRdt = async (code) => {
   }
 }
 
-const injectRdt = async (request, callback) => {
-  let payload = request.payload
-  let author = request.auth.credentials.user
-  let resultForResnpose = []
+// const injectRdt = async (request, callback) => {
+//   let payload = request.payload
+//   let author = request.auth.credentials.user
+//   let resultForResnpose = []
 
-  const process = async () => {
-    for (const payloads of payload) {
-      const result = await returnPayload(payloads)
-      const countRdt = await getCountRdt(result.address_district_code)
+//   const process = async () => {
+//     for (const payloads of payload) {
+//       const result = await returnPayload(payloads)
+//       const countRdt = await getCountRdt(result.address_district_code)
 
-      let date = new Date().getFullYear().toString()
-      let code_test = "PTS-"
-      code_test += countRdt.dinkes_code
-      code_test += date.substr(2, 2)
-      code_test += "0".repeat(5 - countRdt.count.toString().length)
-      code_test += countRdt.count
+//       let date = new Date().getFullYear().toString()
+//       let code_test = "PTS-"
+//       code_test += countRdt.dinkes_code
+//       code_test += date.substr(2, 2)
+//       code_test += "0".repeat(5 - countRdt.count.toString().length)
+//       code_test += countRdt.count
 
-      let code_tool_tester
-      let pcr_count = 0
-      let rdt_count = 0
-      if (result.tool_tester === "PCR") {
-        pcr_count += 1
-        code_tool_tester = "PCR-"
-      } else {
-        rdt_count += 1
-        code_tool_tester = "RDT-"
-      }
-      code_tool_tester += countRdt.dinkes_code
-      code_tool_tester += date.substr(2, 2)
-      code_tool_tester += "0".repeat(5 - countRdt.count.toString().length)
-      code_tool_tester += countRdt.count
+//       let code_tool_tester
+//       let pcr_count = 0
+//       let rdt_count = 0
+//       if (result.tool_tester === "PCR") {
+//         pcr_count += 1
+//         code_tool_tester = "PCR-"
+//       } else {
+//         rdt_count += 1
+//         code_tool_tester = "RDT-"
+//       }
+//       code_tool_tester += countRdt.dinkes_code
+//       code_tool_tester += date.substr(2, 2)
+//       code_tool_tester += "0".repeat(5 - countRdt.count.toString().length)
+//       code_tool_tester += countRdt.count
 
-      let id_case = null
-      let codes = {
-        code_test: (code_test === undefined ? "" : code_test),
-        code_tool_tester: (code_tool_tester === undefined ? "" : code_tool_tester),
-        id_case: (id_case === undefined ? "" : id_case),
-        author_district_code: author.code_district_city,
-        author_district_name: author.name_district_city,
-        rdt_count: rdt_count,
-        pcr_count: pcr_count,
-        source_data: "external"
-      }
+//       let id_case = null
+//       let codes = {
+//         code_test: (code_test === undefined ? "" : code_test),
+//         code_tool_tester: (code_tool_tester === undefined ? "" : code_tool_tester),
+//         id_case: (id_case === undefined ? "" : id_case),
+//         author_district_code: author.code_district_city,
+//         author_district_name: author.name_district_city,
+//         rdt_count: rdt_count,
+//         pcr_count: pcr_count,
+//         source_data: "external"
+//       }
 
-      let rdt = new Rdt(Object.assign(codes, result))
-      rdt = Object.assign(rdt, { author })
-      rdt.save().then(rdt => {
-        let arr = {...codes, ...result}
-        let rdtHistory = new RdtHistory(Object.assign(arr,{rdt}))
-        rdtHistory.save((err, item) => {
-          if(err) console.log(err)
+//       let rdt = new Rdt(Object.assign(codes, result))
+//       rdt = Object.assign(rdt, { author })
+//       rdt.save().then(rdt => {
+//         let arr = {...codes, ...result}
+//         let rdtHistory = new RdtHistory(Object.assign(arr,{rdt}))
+//         rdtHistory.save((err, item) => {
+//           if(err) console.log(err)
 
-          let last_history = { last_history: item._id }
-          rdt = Object.assign(rdt, last_history)
-          return rdt.save()
-        })
-      })
+//           let last_history = { last_history: item._id }
+//           rdt = Object.assign(rdt, last_history)
+//           return rdt.save()
+//         })
+//       })
 
-    }
-  }
+//     }
+//   }
 
-  process().then(() => {
-    return callback(null, resultForResnpose)
-  })
-}
+//   process().then(() => {
+//     return callback(null, resultForResnpose)
+//   })
+// }
 
 
 module.exports = [
@@ -127,9 +127,9 @@ module.exports = [
     name: 'services.inject.lastHistory',
     method: lastHistory
   },
-  {
-    name: 'services.inject.injectRdt',
-    method: injectRdt
-  }
+  // {
+  //   name: 'services.inject.injectRdt',
+  //   method: injectRdt
+  // }
 ];
 
