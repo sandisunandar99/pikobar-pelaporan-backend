4d5b8c84f74906dd7209ace3784f654ab0fbd230
firmanjs <fimzhakim@gmail.com>
2021-07-22 14:26:51 +0700

    add public url aws with expired

--- config/aws.js
+++ config/aws.js
@@ -25,6 +25,26 @@ const readFileFromBucket = async (bucketName, fileName) => {
   return await awsBucket.getObject({ Bucket: bucketName, Key: fileName }).promise()
 }
 
+const getSingedUrl = async (bucketName, fileName) => {
+  const params = {
+    Bucket: bucketName,
+    Key: fileName,
+    Expires: 7200 // expire in 2 hours
+  };
+  try {
+    const url = await new Promise((resolve, reject) => {
+      awsBucket.getSignedUrl('getObject', params, (err, url) => {
+        err ? reject(err) : resolve(url);
+      });
+    });
+    return url
+  } catch (err) {
+    if (err) {
+      console.log(err)
+    }
+  }
+}
+
 module.exports = {
-  uploadFileToAwsBucket, readFileFromBucket
+  uploadFileToAwsBucket, readFileFromBucket, getSingedUrl
 }
